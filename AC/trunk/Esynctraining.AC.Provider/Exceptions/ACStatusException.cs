2687752ecf8fbb1c7994c5b3b8034d4405466913
eugene <eugene@esynctraining.com>
2013-10-30 12:43:30 +0000

    excpetion
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@1875 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ AC/trunk/Esynctraining.AC.Provider/Exceptions/ACStatusException.cs
@@ -0,0 +1,33 @@
+﻿namespace Esynctraining.AC.Provider.Exceptions
+{
+    using System;
+
+    using Esynctraining.AC.Provider.DataObjects.Results;
+
+    /// <summary>
+    /// The ac status exception.
+    /// </summary>
+    public class ACStatusException : Exception
+    {
+        public ACStatusException(ResultBase result)
+        {
+            this.Result = result;
+        }
+
+        /// <summary>
+        /// Gets or sets the result.
+        /// </summary>
+        public ResultBase Result { get; set; }
+
+        /// <summary>
+        /// Gets the message.
+        /// </summary>
+        public override string Message
+        {
+            get
+            {
+                return this.Result == null ? base.Message : this.Result.Status.InnerXml;
+            }
+        }
+    }
+}
