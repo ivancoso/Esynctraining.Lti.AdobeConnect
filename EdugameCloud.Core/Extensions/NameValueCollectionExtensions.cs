b8937014ab4dc147f56b884cf4844c035eab4660
eugene <eugene@esynctraining.com>
2014-01-29 04:47:08 +0000

    missing files
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@3152 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.Core/Extensions/NameValueCollectionExtensions.cs
@@ -0,0 +1,69 @@
+﻿namespace EdugameCloud.Core.Extensions
+{
+    using System;
+    using System.Collections.Specialized;
+    using System.Linq;
+    using System.Web;
+
+    /// <summary>
+    /// Name value collection extensions
+    /// </summary>
+    public static class NameValueCollectionExtensions
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The has key.
+        /// </summary>
+        /// <param name="nvc">
+        /// Name value collection
+        /// </param>
+        /// <param name="key">
+        /// Key value
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public static bool HasKey(this NameValueCollection nvc, string key)
+        {
+            if (nvc != null && nvc.HasKeys())
+            {
+                foreach (var keyVar in nvc.Keys)
+                {
+                    if (keyVar is string && keyVar.ToString().Equals(key, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// The has key.
+        /// </summary>
+        /// <param name="nvc">
+        /// The name value collection.
+        /// </param>
+        /// <param name="key">
+        /// The key.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public static bool HasKey(this HttpCookieCollection nvc, string key)
+        {
+            if (nvc != null && nvc.AllKeys.Length > 0)
+            {
+                return nvc.AllKeys.Any(keyVar => keyVar.Equals(key, StringComparison.InvariantCultureIgnoreCase));
+            }
+
+            return false;
+        }
+
+        #endregion
+    }
+}
