68686583bf79a4d95bba329b9e86748a2f764854
alexander <alexander@esynctraining.com>
2014-02-04 09:31:23 +0000

    Resultant PDF file size optimization
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@3207 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- Core/trunk/Esynctraining.Pdf/Esynctraining.PdfProcessor/PdfProcessorHelper.cs
+++ Core/trunk/Esynctraining.Pdf/Esynctraining.PdfProcessor/PdfProcessorHelper.cs
@@ -165,8 +165,8 @@
                                         result |= processor.Execute(i);
                                     }
                                 }
+                                reader.RemoveUnusedObjects();
                             }
-
                             outPdfBuffer = outPdfStream.ToArray();
                         }
                     }
