dd382262b86905f4ecd04a7bfba90a3390c1e95a
olga <olga@esynctraining.com>
2014-05-23 09:13:48 +0000

    Mail sender changes
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@4129 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.MailsSender/Mail/Models/TrialThirdWeekModel.cs
@@ -0,0 +1,35 @@
+﻿namespace MailSender.Mail.Models
+{
+    using Esynctraining.Core.Providers;
+    using Esynctraining.Core.Providers.Mailer.Models;
+
+    /// <summary>
+    /// The trial model.
+    /// </summary>
+    public class TrialThirdWeekModel : BaseTemplateModel
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TrialThirdWeekModel"/> class. 
+        /// </summary>
+        /// <param name="settings">
+        /// The settings.
+        /// </param>
+        public TrialThirdWeekModel(ApplicationSettingsProvider settings)
+            : base(settings)
+        {
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the user name.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        #endregion
+    }
+}
--- EdugameCloud.MailsSender/TrialMailsSender.cs
+++ EdugameCloud.MailsSender/TrialMailsSender.cs
@@ -1,148 +1,147 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Xml;
-using Castle.MicroKernel.Registration;
-using Castle.Windsor;
-using EdugameCloud.Core.Business.Models;
-using EdugameCloud.Core.Domain.Entities;
-using EdugameCloud.Persistence;
-using EdugameCloud.WCFService.Mail.Models;
-using EdugameCloud.WCFService.Providers;
-using Esynctraining.Core.Business.Models;
-using Esynctraining.Core.Providers;
-using Esynctraining.Core.Providers.Mailer.Models;
-using Esynctraining.Core.Utils;
-using Resources;
-using File = EdugameCloud.Core.Domain.Entities.File;
-
-namespace EdugameCloud.MailSender
+﻿namespace EdugameCloud.MailSender
 {
-	public enum TrialWeeks
-	{
-		First = 0,
-		Second = 7,
-		Third = 14,
-		Fourth = 21
+    using System;
+    using System.Linq;
+    using System.Threading;
 
-	}
-	public class TrialMailsSender
-	{
-		/// <summary>
-		/// Gets the company model.
-		/// </summary>
-		private CompanyLicenseModel CompanyLicenseModel
-		{
-			get
-			{
-				return IoC.Resolve<CompanyLicenseModel>();
-			}
-		}
+    using Castle.Windsor;
 
-		/// <summary>
-		///     Gets the mail model.
-		/// </summary>
-		protected MailModel MailModel
-		{
-			get
-			{
-				return IoC.Resolve<MailModel>();
-			}
-		}
+    using EdugameCloud.Core.Business.Models;
+    using EdugameCloud.Core.Domain.Entities;
+    using EdugameCloud.Persistence;
 
-		/// <summary>
-		///     Gets the settings.
-		/// </summary>
-		protected dynamic Settings
-		{
-			get
-			{
-				return IoC.Resolve<ApplicationSettingsProvider>();
-			}
-		}
+    using Esynctraining.Core.Business.Models;
+    using Esynctraining.Core.Providers;
+    using Esynctraining.Core.Providers.Mailer;
+    using Esynctraining.Core.Utils;
 
-		private static readonly string subject = "Welcome to EduGame Cloud!";
-		private static readonly string nameFrom = "Jacquie";
-		private static readonly string emailFrom = "jbeck@esynctraining.com";
+    using global::MailSender.Mail.Models;
 
-		private void SendMails(TrialWeeks trialWeek)
-		{
-			var trialLicenses = CompanyLicenseModel.GetAll().Where(l => l.LicenseStatus == CompanyLicenseStatus.Trial);
-			foreach (var trialLicense in trialLicenses.Where(l => l.DateCreated.AddDays((int)trialWeek+1).Date == DateTime.Today.Date).ToList())
-			{
-				switch (trialWeek)
-				{
-					case TrialWeeks.First:
-						this.MailModel.SendEmail(
-							trialLicense.CreatedBy.FirstName,
-							trialLicense.CreatedBy.Email,
-							subject,
-							new TrialFirstWeekModel(this.Settings)
-							{
-								FirstName = trialLicense.CreatedBy.FirstName,
-								MailSubject = subject
-							},
-							nameFrom,
-							emailFrom);
-						break;
-					case TrialWeeks.Second:
-						this.MailModel.SendEmail(
-							trialLicense.CreatedBy.FirstName,
-							trialLicense.CreatedBy.Email,
-							subject,
-							new TrialSecondWeekModel(this.Settings)
-							{
-								FirstName = trialLicense.CreatedBy.FirstName,
-								MailSubject = subject
-							},
-							nameFrom,
-							emailFrom);
-						break;
-					case TrialWeeks.Third:
-						this.MailModel.SendEmail(
-							trialLicense.CreatedBy.FirstName,
-							trialLicense.CreatedBy.Email,
-							subject,
-							new TrialThirdWeekModel(this.Settings)
-							{
-								FirstName = trialLicense.CreatedBy.FirstName,
-								MailSubject = subject
-							},
-							nameFrom,
-							emailFrom);
-						break;
-					case TrialWeeks.Fourth:
-						this.MailModel.SendEmail(
-							trialLicense.CreatedBy.FirstName,
-							trialLicense.CreatedBy.Email,
-							subject,
-							new TrialFourthWeekModel(this.Settings)
-							{
-								FirstName = trialLicense.CreatedBy.FirstName,
-								MailSubject = subject
-							},
-							nameFrom,
-							emailFrom);
-						break;
-				}
-				Thread.Sleep(3000);
-			}
-		}
+    /// <summary>
+    /// The trial weeks.
+    /// </summary>
+    public enum TrialWeeks
+    {
+        First = 0,
+        Second = 7,
+        Third = 14,
+        Fourth = 21
+    }
 
-		static void Main()
-		{
-			IoC.Initialize(new WindsorContainer());
-			IoC.Container.RegisterComponents(console:true);
-			TrialMailsSender trialMailsSender = new TrialMailsSender();
-			foreach (TrialWeeks trialWeek in Enum.GetValues(typeof(TrialWeeks)))
-			{
-				trialMailsSender.SendMails(trialWeek);
-			}
-		}
-	}
+    public class TrialMailsSender
+    {
+        /// <summary>
+        ///     Gets the mail model.
+        /// </summary>
+        protected MailModel MailModel
+        {
+            get
+            {
+                return IoC.Resolve<MailModel>();
+            }
+        }
+
+        /// <summary>
+        ///     Gets the settings.
+        /// </summary>
+        protected dynamic Settings
+        {
+            get
+            {
+                return IoC.Resolve<ApplicationSettingsProvider>();
+            }
+        }
+
+        /// <summary>
+        /// Gets the company model.
+        /// </summary>
+        private CompanyLicenseModel CompanyLicenseModel
+        {
+            get
+            {
+                return IoC.Resolve<CompanyLicenseModel>();
+            }
+        }
+
+        private const string Subject = "Welcome to EduGame Cloud!";
+
+        private const string NameFrom = "Jacquie";
+
+        private const string EmailFrom = "jbeck@esynctraining.com";
+
+        private void SendMails(TrialWeeks trialWeek)
+        {
+            var trialLicenses = CompanyLicenseModel.GetAll().Where(l => l.LicenseStatus == CompanyLicenseStatus.Trial);
+            foreach (var trialLicense in trialLicenses.Where(l => l.DateCreated.AddDays((int)trialWeek + 1).Date == DateTime.Today.Date).ToList())
+            {
+                switch (trialWeek)
+                {
+                    case TrialWeeks.First:
+                        this.MailModel.SendEmail(
+                            trialLicense.CreatedBy.FirstName,
+                            trialLicense.CreatedBy.Email,
+                            Subject,
+                            new TrialFirstWeekModel(this.Settings)
+                            {
+                                FirstName = trialLicense.CreatedBy.FirstName,
+                                MailSubject = Subject
+                            },
+                            NameFrom,
+                            EmailFrom);
+                        break;
+                    case TrialWeeks.Second:
+                        this.MailModel.SendEmail(
+                            trialLicense.CreatedBy.FirstName,
+                            trialLicense.CreatedBy.Email,
+                            Subject,
+                            new TrialSecondWeekModel(this.Settings)
+                            {
+                                FirstName = trialLicense.CreatedBy.FirstName,
+                                MailSubject = Subject
+                            },
+                            NameFrom,
+                            EmailFrom);
+                        break;
+                    case TrialWeeks.Third:
+                        this.MailModel.SendEmail(
+                            trialLicense.CreatedBy.FirstName,
+                            trialLicense.CreatedBy.Email,
+                            Subject,
+                            new TrialThirdWeekModel(this.Settings)
+                            {
+                                FirstName = trialLicense.CreatedBy.FirstName,
+                                MailSubject = Subject
+                            },
+                            NameFrom,
+                            EmailFrom);
+                        break;
+                    case TrialWeeks.Fourth:
+                        this.MailModel.SendEmail(
+                            trialLicense.CreatedBy.FirstName,
+                            trialLicense.CreatedBy.Email,
+                            Subject,
+                            new TrialFourthWeekModel(this.Settings)
+                            {
+                                FirstName = trialLicense.CreatedBy.FirstName,
+                                MailSubject = Subject
+                            },
+                            NameFrom,
+                            EmailFrom);
+                        break;
+                }
+                Thread.Sleep(3000);
+            }
+        }
+
+        public static void Main()
+        {
+            IoC.Initialize(new WindsorContainer());
+            IoC.Container.RegisterComponents(console: true);
+            var trialMailsSender = new TrialMailsSender();
+            foreach (TrialWeeks trialWeek in Enum.GetValues(typeof(TrialWeeks)))
+            {
+                trialMailsSender.SendMails(trialWeek);
+            }
+        }
+    }
 }
