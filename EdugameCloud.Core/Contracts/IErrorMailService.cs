a6f9584e22a505f33b70eb0dea0122cc00966421
olga <olga@esynctraining.com>
2014-06-02 09:33:20 +0000

    error mail sender service
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@4203 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.Core/Contracts/IErrorMailService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EdugameCloud.Core.Contracts
+{
+    using System.ServiceModel;
+
+    using Esynctraining.Core.Domain.Contracts;
+
+    /// <summary>
+    ///     The AccountService interface.
+    /// </summary>
+    [ServiceContract]
+    public interface IErrorMailService
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The all.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ServiceResponse"/>.
+        /// </returns>
+        [OperationContract]
+        ServiceResponse SendMailAboutError(string message, string details);
+
+        #endregion
+    }
+}
---
+++ EdugameCloud.WCFService/services/EdugameCloud.WCFService.ErrorMailService.svc.cs
@@ -0,0 +1,52 @@
+﻿// ReSharper disable once CheckNamespace
+namespace EdugameCloud.WCFService
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.ServiceModel;
+    using System.ServiceModel.Activation;
+
+    using EdugameCloud.Core.Contracts;
+    using EdugameCloud.WCFService.Base;
+    using EdugameCloud.WCFService.Mail.Models;
+
+    using Esynctraining.Core.Domain.Contracts;
+
+    using Resources;
+
+    /// <summary>
+    ///     The account service.
+    /// </summary>
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.PerSession,
+        IncludeExceptionDetailInFaults = true)]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
+    public class ErrorMailService : BaseService, IErrorMailService
+    {
+        #region Properties
+
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public ServiceResponse SendMailAboutError(string message, string details)
+        {
+            var emails = ((string)(this.Settings.ErrorEmailRecipients ?? string.Empty))
+                .Split(";".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            this.MailModel.SendEmail(
+                new List<string>(),
+                emails,
+                Emails.ErrorSubject,
+                new ErrorMailModel(this.Settings) { ErrorDetails = details, ErrorMessage = message, MailSubject = Emails.ErrorSubject },
+                Common.AppEmailName,
+                Common.AppEmail);
+            
+            return new ServiceResponse();
+        }
+
+        #endregion
+    }
+}
