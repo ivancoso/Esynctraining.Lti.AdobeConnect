f60c681b8aed3ab474f520a3b1ea42f25bdd8cae
eugene <eugene@esynctraining.com>
2014-11-11 10:26:32 +0000

    db changes
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@5786 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.DB/dbo/Tables/LmsUser.sql
+++ EdugameCloud.DB/dbo/Tables/LmsUser.sql
@@ -6,9 +6,12 @@
     [password]         NVARCHAR (50)  NULL,
     [token]            NVARCHAR (100) NULL,
     [acConnectionMode] INT            CONSTRAINT [DF_LmsUser_acConnectionMode] DEFAULT ((0)) NOT NULL,
+    [primaryColor]     NVARCHAR (50)  NULL,
     CONSTRAINT [PK_LmsUser] PRIMARY KEY CLUSTERED ([lmsUserId] ASC),
     CONSTRAINT [FK_LmsUser_CompanyLms] FOREIGN KEY ([companyLmsId]) REFERENCES [dbo].[CompanyLms] ([companyLmsId])
 );
+
+
 
 
 
