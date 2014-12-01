c6fee81324152257e7375ed033fcf01fb189bb0a
olga <olga@esynctraining.com>
2014-12-01 16:38:11 +0000

    field result, field parser
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@5912 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ AC/trunk/Esynctraining.AC.Provider/DataObjects/Results/FieldResult.cs
@@ -0,0 +1,43 @@
+﻿namespace Esynctraining.AC.Provider.DataObjects.Results
+{
+    using System;
+
+    using Esynctraining.AC.Provider.Entities;
+
+    /// <summary>
+    /// The field result.
+    /// </summary>
+    public class FieldResult : ResultBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FieldResult"/> class.
+        /// </summary>
+        /// <param name="status">
+        /// The status.
+        /// </param>
+        public FieldResult(StatusInfo status)
+            : base(status)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FieldResult"/> class.
+        /// </summary>
+        /// <param name="status">
+        /// The status.
+        /// </param>
+        /// <param name="fieldValue">
+        /// The field value.
+        /// </param>
+        public FieldResult(StatusInfo status, String fieldValue)
+            : base(status)
+        {
+            this.FieldValue = fieldValue;
+        }
+
+        /// <summary>
+        /// Gets or sets the field value.
+        /// </summary>
+        public String FieldValue { get; set; }
+    }
+}
---
+++ AC/trunk/Esynctraining.AC.Provider/EntityParsing/FieldParser.cs
@@ -0,0 +1,33 @@
+﻿namespace Esynctraining.AC.Provider.EntityParsing
+{
+    using System;
+    using System.Xml;
+
+    using Esynctraining.AC.Provider.Extensions;
+
+    /// <summary>
+    /// The field parser.
+    /// </summary>
+    internal static class FieldParser
+    {
+        /// <summary>
+        /// The path.
+        /// </summary>
+        private const string Path = "//results//acl-fields//field//value";
+
+        /// <summary>
+        /// Parses the specified XML.
+        /// </summary>
+        /// <param name="xml">The XML.</param>
+        /// <returns>EventInfo object.</returns>
+        public static String Parse(XmlNode xml)
+        {
+            if (xml == null || !xml.NodeListExists(Path))
+            {
+                return null;
+            }
+
+            return xml.SelectSingleNode(Path).SelectSingleNodeValue("text()");
+        }
+    }
+}
