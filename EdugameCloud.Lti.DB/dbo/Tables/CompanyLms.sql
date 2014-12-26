75ba5f70257d1e6443d58da0fe1513487491f8cd
eugene <eugene@esynctraining.com>
2014-12-26 05:08:22 +0000

    Lti DB Freeze
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@6032 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.Lti.DB/dbo/Tables/CompanyLms.sql
@@ -0,0 +1,37 @@
+﻿CREATE TABLE [dbo].[CompanyLms] (
+    [companyLmsId]      INT            IDENTITY (1, 1) NOT NULL,
+    [companyId]         INT            NOT NULL,
+    [lmsProviderId]     INT            NOT NULL,
+    [acServer]          NVARCHAR (100) NOT NULL,
+    [acUsername]        NVARCHAR (100) NOT NULL,
+    [acPassword]        NVARCHAR (100) NOT NULL,
+    [consumerKey]       NVARCHAR (100) NOT NULL,
+    [sharedSecret]      NVARCHAR (100) NOT NULL,
+    [createdBy]         INT            NOT NULL,
+    [modifiedBy]        INT            NULL,
+    [dateCreated]       DATE           NOT NULL,
+    [dateModified]      DATE           NULL,
+    [layout]            NVARCHAR (50)  NULL,
+    [primaryColor]      NVARCHAR (50)  NULL,
+    [adminUserId]       INT            NULL,
+    [acScoId]           NVARCHAR (50)  NULL,
+    [acTemplateScoId]   NVARCHAR (50)  NULL,
+    [lmsDomain]         NVARCHAR (100) NULL,
+    [showAnnouncements] BIT            NULL,
+    [title]             NVARCHAR (100) NULL,
+    [useSSL]            BIT            NULL,
+    [lastSignalId]      BIGINT         CONSTRAINT [DF_CompanyLms_lastSignalId] DEFAULT ((0)) NOT NULL,
+    [useUserFolder]     BIT            NULL,
+    [userFolderName]    NVARCHAR (50)  NULL,
+    [canRemoveMeeting]  BIT            NULL,
+    [canEditMeeting]    BIT            NULL,
+    [isSettingsVisible] BIT            NULL,
+    CONSTRAINT [PK_CompanyLms] PRIMARY KEY CLUSTERED ([companyLmsId] ASC),
+    CONSTRAINT [FK_CompanyLms_LmsProvider] FOREIGN KEY ([lmsProviderId]) REFERENCES [dbo].[LmsProvider] ([lmsProviderId]),
+    CONSTRAINT [FK_CompanyLms_LmsUser] FOREIGN KEY ([adminUserId]) REFERENCES [dbo].[LmsUser] ([lmsUserId])
+);
+
+
+
+
+
---
+++ EdugameCloud.Lti.DB/dbo/Tables/LmsProvider.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[LmsProvider] (
+    [lmsProviderId] INT           NOT NULL,
+    [lmsProvider]   NVARCHAR (50) NULL,
+    [shortName]     NVARCHAR (50) NULL,
+    CONSTRAINT [PK_LmsProvider_1] PRIMARY KEY CLUSTERED ([lmsProviderId] ASC)
+);
+
+
+
