eadc7b2599bb5cd11b9956064a3608cff527aa00
sergey <sergeyi@esynctraining.com>
2015-02-13 16:34:33 +0000

    clean-up: removed unused 'usings'
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@6287 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.Lti.Persistence/FluentConfiguration.cs
+++ EdugameCloud.Lti.Persistence/FluentConfiguration.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace EdugameCloud.Lti.Persistence
+﻿namespace EdugameCloud.Lti.Persistence
 {
     using System.Reflection;
 
