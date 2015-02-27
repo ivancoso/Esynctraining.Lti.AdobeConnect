486a6cfa314b4c762876605011c60a91a786d3d4
eugene <eugene@esynctraining.com>
2015-02-27 10:43:21 +0000

    StringArrayTypeAdapter
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@6385 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ Core/trunk/DotAmf/Serialization/TypeAdapters/StringArrayTypeAdapter.cs
@@ -0,0 +1,100 @@
+﻿namespace DotAmf.Serialization.TypeAdapters
+{
+    using System;
+    using System.Collections;
+    using System.Globalization;
+    using System.Linq;
+
+    /// <summary>
+    ///     The float null type adapter.
+    /// </summary>
+    public class StringArrayTypeAdapter : BaseTypeAdapter<string[]>
+    {
+        #region Static Fields
+
+        /// <summary>
+        /// The custom format.
+        /// </summary>
+        private static readonly NumberFormatInfo CustomFormat = new NumberFormatInfo
+                                                                    {
+                                                                        NegativeSign = "-", 
+                                                                        NumberDecimalSeparator = ".", 
+                                                                        NumberGroupSeparator = ",", 
+                                                                        CurrencySymbol = "$", 
+                                                                        CurrencyDecimalSeparator = ".", 
+                                                                        CurrencyGroupSeparator = ",", 
+                                                                    };
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The adapt.
+        /// </summary>
+        /// <param name="type">
+        /// The type.
+        /// </param>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="object"/>.
+        /// </returns>
+        public override object Adapt(Type type, object value)
+        {
+            if (value is float[])
+            {
+                return this.Convert(value);
+            }
+
+            if (value is int[])
+            {
+                return this.Convert(value);
+            }
+
+            if (value is double[])
+            {
+                return this.Convert(value);
+            }
+
+            if (value is bool[])
+            {
+                return this.Convert(value);
+            }
+
+            return value;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// The convert.
+        /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="decimal"/>.
+        /// </returns>
+        private string[] Convert(object value)
+        {
+            if (value != null)
+            {
+                var type = value.GetType();
+                if (type.IsArray)
+                {
+                    return ((IEnumerable)value).Cast<object>()
+                                 .Select(x => x.ToString())
+                                 .ToArray();
+                }
+            }
+
+            return null;
+        }
+
+        #endregion
+    }
+}
