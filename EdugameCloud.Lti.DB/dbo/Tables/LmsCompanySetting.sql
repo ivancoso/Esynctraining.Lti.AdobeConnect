c2376d44877115634a6de242fa78cd479ea6a921
ivan <ivan@esynctraining.com>
2015-04-03 10:00:18 +0000

    DB updates - LmsCompanySetting table
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@6685 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.Lti.DB/dbo/Tables/LmsCompanySetting.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[LmsCompanySetting] (
+    [lmsCompanySettingId] INT            IDENTITY (1, 1) NOT NULL,
+    [lmsCompanyId]        INT            NOT NULL,
+    [name]                NVARCHAR (100) NOT NULL,
+    [value]               NVARCHAR (200) NOT NULL,
+    CONSTRAINT [PK_LmsCompanySetting] PRIMARY KEY CLUSTERED ([lmsCompanySettingId] ASC),
+    CONSTRAINT [FK_LmsCompanySetting_LmsCompany] FOREIGN KEY ([lmsCompanyId]) REFERENCES [dbo].[CompanyLms] ([companyLmsId]) ON DELETE CASCADE
+);
+
