1dd1706da82e3367cf61e05b0517a0899f913cab
sergey <sergeyi@esynctraining.com>
2015-04-03 09:05:22 +0000

    Fixed IDesire2LearnApiService registration
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@6683 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.Web/Global.asax.cs
+++ EdugameCloud.Web/Global.asax.cs
@@ -93,13 +93,7 @@ namespace EdugameCloud.Web
             container.Register(Component.For<MeetingSetup>().ImplementedBy<MeetingSetup>());
             container.Register(Component.For<UsersSetup>().ImplementedBy<UsersSetup>());
             container.Register(Classes.FromAssemblyNamed("EdugameCloud.Lti").Pick().If(Component.IsInNamespace("EdugameCloud.Lti.Controllers")).WithService.Self().LifestyleTransient());
-            container.Register(Component.For<IDesire2LearnApiService>().ImplementedBy<Desire2LearnApiService>()
-                .DynamicParameters((k, d) =>
-                {
-                    var settings = k.Resolve<ApplicationSettingsProvider>();
-                    d["providerKey"] = ((dynamic)settings).D2LApiKey;
-                    d["providerSecret"] = ((dynamic)settings).D2LApiSecret;
-                }).LifestyleTransient());
+            container.Register(Component.For<IDesire2LearnApiService>().ImplementedBy<Desire2LearnApiService>().LifestyleTransient());
         }
 
         /// <summary>
