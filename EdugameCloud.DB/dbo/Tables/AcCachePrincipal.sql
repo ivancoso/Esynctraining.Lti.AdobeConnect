2c1b0ed7c92ef6c40c044dd318388655f6ff45f8
sergey <sergeyi@esynctraining.com>
2015-04-23 18:03:02 +0000

    + Tables\AcCachePrincipal.sql; + Stored Procedures\getUsersByLmsCompanyId.sql
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@6900 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.DB/dbo/Tables/AcCachePrincipal.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[AcCachePrincipal]
+(
+	[acCachePrincipalId]	INT		NOT NULL	IDENTITY (1, 1),
+
+	[lmsCompanyId]			INT		NOT NULL,
+
+	[accountId]		NVARCHAR(512)		NULL,
+	[displayId]		NVARCHAR(512)		NULL,
+	[email]			NVARCHAR(512)		NULL,
+	[firstName]		NVARCHAR(512)		NULL,
+	[hasChildren]	BIT					NULL,
+	[isHidden]		BIT					NULL,
+	[isPrimary]		BIT					NULL,
+    [lastName]		NVARCHAR(512)		NULL,
+	[login]			NVARCHAR(512)		NULL,
+	[name]			NVARCHAR(512)		NULL,
+	[principalId]	NVARCHAR(512)		NULL,
+	[type]			NVARCHAR(512)		NULL,
+
+	CONSTRAINT [PK_AcCachePrincipal] PRIMARY KEY CLUSTERED ([acCachePrincipalId] ASC),
+	CONSTRAINT [FK_AcCachePrincipal_CompanyLms] FOREIGN KEY ([lmsCompanyId]) REFERENCES [dbo].[CompanyLms] ([companyLmsId]) ON DELETE CASCADE
+)
