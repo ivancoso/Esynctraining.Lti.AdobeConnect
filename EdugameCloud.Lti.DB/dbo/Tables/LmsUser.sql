6fda32d368d0a1d8472ce35cd5b53082e6d28267
ivan <ivan@esynctraining.com>
2015-05-25 16:12:40 +0000

    User synchronization - db scripts
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@7321 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.Lti.DB/dbo/Tables/LmsUser.sql
+++ EdugameCloud.Lti.DB/dbo/Tables/LmsUser.sql
@@ -8,9 +8,14 @@
     [acConnectionMode] INT            CONSTRAINT [DF_LmsUser_acConnectionMode] DEFAULT ((0)) NULL,
     [primaryColor]     NVARCHAR (50)  NULL,
     [principalId]      NVARCHAR (30)  NULL,
+    [name]             NVARCHAR (100) NULL,
+    [email]            NVARCHAR (100) NULL,
+    [userIdExtended]   NVARCHAR (50)  NULL,
     CONSTRAINT [PK_LmsUser] PRIMARY KEY CLUSTERED ([lmsUserId] ASC),
     CONSTRAINT [FK_LmsUser_CompanyLms] FOREIGN KEY ([companyLmsId]) REFERENCES [dbo].[CompanyLms] ([companyLmsId]) ON DELETE CASCADE
 );
+
+
 
 
 
