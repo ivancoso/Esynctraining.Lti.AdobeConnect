291c094e4080cf759d76ead1bc63c6b6b5697035
sergey <sergeyi@esynctraining.com>
2015-06-30 08:33:09 +0000

    + FKeys
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@7757 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.DB/dbo/Tables/LmsCourseMeeting.sql
+++ EdugameCloud.DB/dbo/Tables/LmsCourseMeeting.sql
@@ -12,6 +12,8 @@
 
 	CONSTRAINT [PK_LmsCourseMeeting] PRIMARY KEY CLUSTERED ([lmsCourseMeetingId] ASC),
 	CONSTRAINT [FK_LmsCourseMeeting_LmsMeetingType] FOREIGN KEY ([lmsMeetingTypeId]) REFERENCES [dbo].[LmsMeetingType] ([lmsMeetingTypeId]),
-	CONSTRAINT [FK_LmsCourseMeeting_CompanyLms] FOREIGN KEY ([companyLmsId]) REFERENCES [dbo].[CompanyLms] ([companyLmsId])
+	CONSTRAINT [FK_LmsCourseMeeting_CompanyLms] FOREIGN KEY ([companyLmsId]) REFERENCES [dbo].[CompanyLms] ([companyLmsId]),
+	CONSTRAINT [FK_LmsCourseMeeting_OfficeHours] FOREIGN KEY ([officeHoursId]) REFERENCES [dbo].[OfficeHours] ([officeHoursId]),
+	CONSTRAINT [FK_LmsCourseMeeting_LmsUser] FOREIGN KEY ([ownerId]) REFERENCES [dbo].[LmsUser] ([lmsUserId])
 );
 GO
--- EdugameCloud.DB/dbo/Tables/OfficeHours.sql
+++ EdugameCloud.DB/dbo/Tables/OfficeHours.sql
@@ -1,8 +1,12 @@
 ﻿CREATE TABLE [dbo].[OfficeHours] (
-    [officeHoursId] INT            IDENTITY (1, 1) NOT NULL,
-    [hours]         NVARCHAR (100) NULL,
-    [scoId]         NVARCHAR (50)  NOT NULL,
-    [lmsUserId]     INT            NOT NULL,
-    CONSTRAINT [PK_OfficeHours] PRIMARY KEY CLUSTERED ([officeHoursId] ASC)
+	[officeHoursId]		INT				NOT NULL	IDENTITY(1, 1),
+	[hours]				NVARCHAR(100)		NULL,
+	[scoId]				NVARCHAR(50)	NOT NULL,
+	[lmsUserId]			INT				NOT NULL,
+	CONSTRAINT [PK_OfficeHours] PRIMARY KEY CLUSTERED ([officeHoursId] ASC),
+	CONSTRAINT [FK_OfficeHours_LmsUser] FOREIGN KEY ([lmsUserId]) REFERENCES [dbo].[LmsUser] ([lmsUserId])
 );
+GO
 
+CREATE UNIQUE NONCLUSTERED INDEX [UI_OfficeHours_lmsUserId] ON [dbo].[OfficeHours] ([lmsUserId])
+GO
