00c74da9f52dada16752946b0928d47ffdfc8493
ivan <ivan@esynctraining.com>
2015-07-29 12:19:35 +0000

    EDUGAMECLOUD-1438 stored procedure
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@8066 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.DB/dbo/Stored Procedures/getSurveyResultAnswers.sql
@@ -0,0 +1,31 @@
+﻿USE [EduGameCloud]
+GO
+/****** Object:  StoredProcedure [dbo].[getSurveyResultAnswers]    Script Date: 29.07.2015 12:26:31 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[getSurveyResultAnswers]
+(
+	@surveyResultIds AS XML
+)
+AS
+BEGIN
+	WITH UserIdsCTE AS
+	(
+		SELECT x.i.value('.[1]', 'int') as Id
+		FROM @surveyResultIds.nodes('//Ids/Id') as x(i)
+	)
+	select sqra.surveyQuestionResultAnswerId,
+		sqra.surveyQuestionResultId,
+		sqra.value,
+		sqra.surveyDistractorId,
+		sqra.surveyDistractorAnswerId,
+		ISNULL(sqr.questionId,0) as questionId, 
+		ISNULL(sqr.questionTypeId,0) as questionTypeId
+from SurveyQuestionResultAnswer sqra
+	inner join UserIdsCTE ON UserIdsCTE.Id = sqra.surveyQuestionResultId
+	left join SurveyQuestionResult sqr on sqra.surveyQuestionResultId = sqr.surveyQuestionResultId
+END
+
+select * from SurveyQuestionResultAnswer
