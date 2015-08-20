5e64b828651b9896c25433c305c0ad129e467e60
sergey <sergeyi@esynctraining.com>
2015-08-20 07:21:28 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@8336 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.Lti.ConnectionTest/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using Castle.Core.Logging;
+using EdugameCloud.Core.Business.Models;
+using EdugameCloud.Core.Domain.Entities;
+using EdugameCloud.Lti.API;
+using EdugameCloud.Lti.Core.Business.Models;
+using EdugameCloud.Lti.Domain.Entities;
+using Esynctraining.Core.Utils;
+
+namespace EdugameCloud.Lti.ConnectionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IoCStart.Init();
+            ILogger logger = IoC.Resolve<ILogger>();
+            try
+            {
+                logger.InfoFormat("===== ConnectionTest Starts. DateTime:{0} =====", DateTime.Now);
+                LmsFactory lmsFactory = IoC.Resolve<LmsFactory>();
+                var lmsCompanyModel = IoC.Resolve<LmsCompanyModel>();
+                var syncService = IoC.Resolve<ISynchronizationUserService>();
+
+                var licenses = lmsCompanyModel.GetAll();
+
+                var tester = IoC.Resolve<TestConnectionService>();
+
+                foreach (var lmsCompany in licenses)
+                {
+                        try
+                        {
+                           // syncService.SynchronizeUsers(lmsCompany, syncACUsers: true);
+                        }
+                        catch (Exception ex)
+                        {
+                            logger.ErrorFormat(ex, "Unexpected error during execution for LmsCompanyId: {0}.", lmsCompany.Id);
+                        }
+                    
+                }
+
+            }
+            catch (Exception ex)
+            {
+                string msg = "Unexpected error during execution ConnectionTest with message: " + ex.Message;
+                logger.Error(msg, ex);
+            }
+            finally
+            {
+                logger.InfoFormat("===== ConnectionTest stops. DateTime:{0} =====", DateTime.Now);
+            }
+        }
+
+    }
+
+}
