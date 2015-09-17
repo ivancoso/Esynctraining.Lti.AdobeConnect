6e40d181c03f20114bbbf2a3b3063529ef13fdc7
ivan <ivan@esynctraining.com>
2015-09-17 08:15:53 +0000

    removed unnecessary properties
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@8655 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.Lti.Core/DTO/LtiSessionDTO.cs
+++ EdugameCloud.Lti.Core/DTO/LtiSessionDTO.cs
@@ -5,6 +5,7 @@
 
     using EdugameCloud.Lti.Utils;
 
+    //todo: remove this class
     /// <summary>
     /// The LTI session DTO.
     /// </summary>
@@ -15,36 +16,10 @@
         #region Public Properties
 
         /// <summary>
-        /// Gets or sets the AC password data.
-        /// </summary>
-        [DataMember]
-        public virtual string ACPasswordData { get; set; }
-
-        /// <summary>
         /// Gets or sets the LTI param.
         /// </summary>
         [DataMember]
         public virtual LtiParamDTO LtiParam { get; set; }
-
-        /// <summary>
-        /// Gets or sets the shared key.
-        /// </summary>
-        [DataMember]
-        public virtual string SharedKey { get; set; }
-
-        /// <summary>
-        /// Gets the restored AC password.
-        /// </summary>
-        [DataMember]
-        public virtual string RestoredACPassword
-        {
-            get
-            {
-                return string.IsNullOrWhiteSpace(this.ACPasswordData) || string.IsNullOrWhiteSpace(this.SharedKey)
-                           ? null
-                           : AESGCM.SimpleDecrypt(this.ACPasswordData, Convert.FromBase64String(this.SharedKey));
-            }
-        }
 
         #endregion
     }
