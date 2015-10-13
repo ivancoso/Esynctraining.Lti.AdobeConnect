e5db3bd230addd29d87ce71b61cbcf48f540f2a6
sergey <sergeyi@esynctraining.com>
2015-10-13 08:30:00 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@8914 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ CastleLog4Net/Log4NetFactory.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using Castle.Core.Logging;
+using log4net;
+using log4net.Config;
+
+namespace Esynctraining.CastleLog4Net
+{
+    internal sealed class Log4NetFactory : Castle.Core.Logging.AbstractLoggerFactory
+    {
+        internal const string DefaultConfigFileName = "log4net.cfg.xml";
+
+
+        public Log4NetFactory()
+            : this(DefaultConfigFileName)
+        {
+        }
+
+
+        public Log4NetFactory(string configFile)
+        {
+            var file = GetConfigFile(configFile);
+            XmlConfigurator.ConfigureAndWatch(file);
+        }
+
+        public Log4NetFactory(bool configuredExternally)
+        {
+            if (configuredExternally)
+            {
+                return;
+            }
+
+            var file = GetConfigFile(DefaultConfigFileName);
+            XmlConfigurator.ConfigureAndWatch(file);
+        }
+
+        public Log4NetFactory(Stream config)
+        {
+            XmlConfigurator.Configure(config);
+        }
+
+        public override ILogger Create(Type type)
+        {
+            if (type == null)
+                throw new ArgumentNullException("type");
+            var log = LogManager.GetLogger(type);
+            return new Log4netLogger(log, this);
+        }
+
+        public override ILogger Create(Type type, LoggerLevel level)
+        {
+            throw new NotSupportedException("Logger levels cannot be set at runtime. Please review your configuration file.");
+        }
+
+        public override ILogger Create(string name)
+        {
+            if (name == null)
+                throw new ArgumentNullException("name");
+            var log = LogManager.GetLogger(name);
+            return new Log4netLogger(log, this);
+        }
+
+        public override ILogger Create(string name, LoggerLevel level)
+        {
+            throw new NotSupportedException("Logger levels cannot be set at runtime. Please review your configuration file.");
+        }
+
+    }
+
+}
---
+++ CastleLog4Net/LoggerWindsorInstaller.cs
@@ -0,0 +1,21 @@
+﻿using Castle.Facilities.Logging;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace Esynctraining.CastleLog4Net
+{
+    public sealed class LoggerWindsorInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.AddFacility<LoggingFacility>(x =>
+                x
+                //.WithConfig(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile)
+                //.ToLog("MyLogger")
+                .LogUsing<Log4NetFactory>());
+        }
+
+    }
+
+}
