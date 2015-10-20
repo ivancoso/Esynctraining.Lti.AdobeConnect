8053acea46c40fe8d8629173aea5a046b5c9af07
sergey <sergeyi@esynctraining.com>
2015-10-20 11:35:32 +0000

    cache languages
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@8996 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.Core/Business/CachePolicies.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using Esynctraining.Core.Providers;
+using Esynctraining.Core.Utils;
+
+namespace EdugameCloud.Core.Business
+{
+    // TODO: find better place\name
+    public static class CachePolicies
+    {
+        private static readonly string _cacheDependencyFolderPath;
+
+
+        static CachePolicies()
+        {
+            _cacheDependencyFolderPath = (string)((dynamic)IoC.Resolve<ApplicationSettingsProvider>()).Cache_CacheDependencyFolder;
+            _cacheDependencyFolderPath = _cacheDependencyFolderPath.TrimEnd('\\');
+        }
+
+
+        public static class Keys
+        {
+            public static string Countries()
+            {
+                return "Countries";
+            }
+
+            public static string Languages()
+            {
+                return "Languages";
+            }
+            
+            public static string PasswordPolicies(string apiUrl)
+            {
+                return apiUrl;
+            }
+
+            public static string IsActiveCompany(int companyId)
+            {
+                return string.Concat("C_", companyId.ToString());
+            }
+
+            public static string CompanyLmsSettings(int companyLmsId)
+            {
+                return string.Concat("LMS_", companyLmsId.ToString());
+            }
+
+            public static string LmsProviders()
+            {
+                return "LP_";
+            }
+
+        }
+
+        public static class Dependencies
+        {
+            public static string IsActiveCompany(int companyId)
+            {
+                return string.Concat(_cacheDependencyFolderPath, @"\C_", companyId.ToString(), ".cache");
+            }
+
+            public static string CompanyLmsSettings(int companyLmsId)
+            {
+                return string.Concat(_cacheDependencyFolderPath, @"\LMS_", companyLmsId.ToString(), ".cache");
+            }
+            
+        }
+
+        public static void InvalidateCache(string dependencyPath)
+        {
+            //File.Delete(dependencyPath);
+            File.WriteAllText(dependencyPath, Guid.NewGuid().ToString());
+        }
+
+        public static void InvalidateCache()
+        {
+            Array.ForEach(Directory.GetFiles(_cacheDependencyFolderPath), (path) => File.WriteAllText(path, Guid.NewGuid().ToString()));
+        }
+
+    }
+
+}
--- EdugameCloud.Core/Business/Models/LanguageModel.cs
+++ EdugameCloud.Core/Business/Models/LanguageModel.cs
@@ -1,15 +1,20 @@
 ﻿namespace EdugameCloud.Core.Business.Models
 {
+    using System.Collections.Generic;
     using EdugameCloud.Core.Domain.Entities;
 
     using Esynctraining.Core.Business;
     using Esynctraining.Core.Business.Models;
+    using Esynctraining.Core.Business.Queries;
+    using Esynctraining.Core.Caching;
 
     /// <summary>
-    ///     The language model.
+    /// The language model.
     /// </summary>
     public class LanguageModel : BaseModel<Language, int>
     {
+        private readonly ICache _cache;
+
         #region Constructors and Destructors
 
         /// <summary>
@@ -18,11 +23,23 @@
         /// <param name="repository">
         /// The repository.
         /// </param>
-        public LanguageModel(IRepository<Language, int> repository)
+        public LanguageModel(IRepository<Language, int> repository, ICache cache)
             : base(repository)
         {
+            _cache = cache;
         }
 
         #endregion
+
+        public override IEnumerable<Language> GetAll()
+        {
+            return CacheUtility.GetCachedItem<IEnumerable<Language>>(_cache, CachePolicies.Keys.Languages(), () =>
+            {
+                var query = new DefaultQueryOver<Language, int>().GetQueryOver().OrderBy(x => x.LanguageName).Asc;
+                return this.Repository.FindAll(query);
+            });
+        }
+
     }
+
 }
