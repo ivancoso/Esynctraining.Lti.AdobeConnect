6b6becb5e7fb93393a1c01b2989ee820ec01246a
sergey <sergeyi@esynctraining.com>
2015-10-20 06:53:12 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@8983 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.Core.Logging/LoggerWindsorInstaller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Esynctraining.Core.Logging;
+
+namespace EdugameCloud.Core.Logging
+{
+    public sealed class LoggerWindsorInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<ILogger>().ImplementedBy<Logger>().LifeStyle.Singleton);
+        }
+
+    }
+
+}
