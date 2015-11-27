a78aae861b3db9befaf79da230688d682d47acf3
sergey <sergeyi@esynctraining.com>
2015-11-27 16:29:17 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@9243 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ Core/trunk/Esynctraining.Windsor/WindsorServiceLocator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Castle.Windsor;
+using Esynctraining.Core.Utils;
+
+namespace Esynctraining.Windsor
+{
+    /// <summary>
+    /// Adapts the behavior of the Windsor container to the common
+    /// IServiceLocator
+    /// </summary>
+    public class WindsorServiceLocator : IServiceLocator
+    {
+        private readonly IWindsorContainer container;
+
+
+        public WindsorServiceLocator(IWindsorContainer container)
+        {
+            this.container = container;
+        }
+
+
+        public object GetInstance(Type serviceType)
+        {
+            return container.Resolve(serviceType);
+        }
+
+        public object GetInstance(Type serviceType, string key)
+        {
+            return container.Resolve(serviceType, key);
+        }
+
+        public TService GetInstance<TService>()
+        {
+            return container.Resolve<TService>();
+        }
+
+        public TService GetInstance<TService>(string key)
+        {
+            return container.Resolve<TService>(key);
+        }
+
+    }
+
+}
