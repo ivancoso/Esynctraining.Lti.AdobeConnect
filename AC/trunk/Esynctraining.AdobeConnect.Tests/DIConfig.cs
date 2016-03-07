3b63b98bcd2b8cb3a950f8c709876b6aef51a0bb
sergey <sergeyi@esynctraining.com>
2016-03-07 11:11:14 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@10154 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ AC/trunk/Esynctraining.AdobeConnect.Tests/DIConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.Core.Resource;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Esynctraining.CastleLog4Net;
+using Esynctraining.Core.Providers;
+
+namespace Esynctraining.AdobeConnect.Tests
+{
+    public class DIConfig
+    {
+        public static void RegisterComponents(IWindsorContainer container)
+        {
+            container.Install(new LoggerWindsorInstaller());
+            container.Install(new Esynctraining.Core.Logging.CastleLogger.LoggerWindsorInstaller());
+
+            container.Install(
+                Castle.Windsor.Installer.Configuration.FromXml(new AssemblyResource("assembly://Esynctraining.Core/Esynctraining.Core.Windsor.xml"))
+            //Castle.Windsor.Installer.Configuration.FromXml(new AssemblyResource("assembly://Esynctraining.Mail/Esynctraining.Mail.Windsor.xml"))
+            );
+
+            container.Install(
+                Castle.Windsor.Installer.Configuration.FromXml(new AssemblyResource("assembly://Esynctraining.AdobeConnect/Esynctraining.AdobeConnect.Windsor.xml"))
+            );
+
+            container.Register(Component.For<ApplicationSettingsProvider>().ImplementedBy<ApplicationSettingsProvider>()
+                    .DynamicParameters((k, d) => d.Add("collection", ConfigurationManager.AppSettings))
+                    .LifeStyle.Singleton);
+        }
+
+    }
+
+
+}
---
+++ AC/trunk/Esynctraining.AdobeConnect.Tests/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.Windsor;
+using Esynctraining.AC.Provider.Entities;
+using Esynctraining.Core.Logging;
+using Esynctraining.Core.Utils;
+using Esynctraining.Windsor;
+
+namespace Esynctraining.AdobeConnect.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new WindsorContainer();
+            WindsorIoC.Initialize(container);
+            DIConfig.RegisterComponents(container);
+
+            var logger = IoC.Resolve<ILogger>();
+            var accountService = new AdobeConnectAccountService(logger);
+            IAdobeConnectProxy ac = accountService.GetProvider(new AdobeConnectAccess("https://connect.esynctraining.com", "sergeyi@esynctraining.com", "Qrazy123"), true);
+
+            var seminarService = new SeminarService(logger);
+            var licenses = seminarService.GetSeminarLicenses(ac);
+
+            string licenseScoId = licenses.Last().ScoId;
+            var seminars = seminarService.GetSeminars(licenseScoId, ac);
+
+            string seminarScoId = seminars.First(x => x.ScoId == "1266912").ScoId;
+            var sessions = seminarService.GetSeminarSessions(seminarScoId, ac);
+
+            var seminar = new SeminarUpdateItem
+            {
+                ScoId = "1429897",
+                FolderId = licenseScoId,
+                Description = "test dest2 3",
+                Name = "isa tests Api - 3",
+                Type = ScoType.meeting,
+                Language = "en",
+                //UrlPath = "isa",
+            };
+
+            ScoInfo result = seminarService.SaveSeminar(seminar, ac);
+
+            var res = seminarService.SaveSession(new SeminarSessionDto
+            {
+                SeminarSessionScoId = "1429898",
+                SeminarScoId = result.ScoId,
+                Name = "new API 2",
+                DateBegin = DateTime.Now.AddDays(3),
+                DateEnd = DateTime.Now.AddDays(3).AddMinutes(45),
+                ExpectedLoad = 15,
+            }, ac);
+
+            //var resDelete = seminarService.DeleteSesson("1406981", ac);
+
+            //var resDelete = seminarService.DeleteSeminar("1406519", ac);
+        }
+
+    }
+
+}
