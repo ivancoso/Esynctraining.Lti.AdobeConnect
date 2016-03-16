4a5809f14fcf6b0e3bfd6d4b62813da71b46dde1
sergey <sergeyi@esynctraining.com>
2016-03-16 10:10:50 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@10299 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.Lti/Controllers/Mp4ServiceController.cs
+++ EdugameCloud.Lti/Controllers/Mp4ServiceController.cs
@@ -1,229 +1,230 @@
-﻿using System;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web.Mvc;
-using System.Web.UI;
-using EdugameCloud.Lti.API.AdobeConnect;
-using EdugameCloud.Lti.Core;
-using EdugameCloud.Lti.Core.Business.Models;
-using EdugameCloud.Lti.Core.Constants;
-using EdugameCloud.Lti.Core.DTO;
-using EdugameCloud.Lti.Domain.Entities;
-using EdugameCloud.Lti.DTO;
-using EdugameCloud.Lti.Extensions;
-using Esynctraining.Core.Logging;
-using Esynctraining.Core.Providers;
-using Esynctraining.Core.Utils;
-using Esynctraining.Mp4Service.Tasks.Client;
-using Esynctraining.WebApi.Client;
+﻿//using System;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using System.Web.Mvc;
+//using System.Web.UI;
+//using EdugameCloud.Lti.API.AdobeConnect;
+//using EdugameCloud.Lti.Core;
+//using EdugameCloud.Lti.Core.Business.Models;
+//using EdugameCloud.Lti.Core.Constants;
+//using EdugameCloud.Lti.Core.DTO;
+//using EdugameCloud.Lti.Domain.Entities;
+//using EdugameCloud.Lti.DTO;
+//using EdugameCloud.Lti.Extensions;
+//using Esynctraining.Core.Domain;
+//using Esynctraining.Core.Logging;
+//using Esynctraining.Core.Providers;
+//using Esynctraining.Core.Utils;
+//using Esynctraining.Mp4Service.Tasks.Client;
+//using Esynctraining.WebApi.Client;
 
-namespace EdugameCloud.Lti.Controllers
-{
-    public partial class Mp4ServiceController : BaseController
-    {
-        public Mp4ServiceController(
-            LmsUserSessionModel userSessionModel,
-            IAdobeConnectAccountService acAccountService,
-            ApplicationSettingsProvider settings,
-            ILogger logger)
-            : base(userSessionModel, acAccountService, settings, logger)
-        {
-        }
-
-
-        [HttpPost]
-        public virtual async Task<JsonResult> Convert(string lmsProviderName, string recordingId)
-        {
-            LmsCompany lmsCompany = null;
-            try
-            {
-                var session = GetReadOnlySession(lmsProviderName);
-                lmsCompany = session.LmsCompany;
-
-                string licenseKey = lmsCompany.GetSetting<string>(LmsCompanySettingNames.Mp4ServiceLicenseKey);
-                if (string.IsNullOrWhiteSpace(licenseKey))
-                    throw new WarningMessageException("Can't find your MP4Service licence. Contact administrator.");
-
-                var mp4Client = IoC.Resolve<Mp4ServiceTaskClient>();
-                MP4Service.Contract.Client.DataTask task = null;
-                try
-                {
-                    var license = await mp4Client.GetLicense(new Guid(licenseKey)).ConfigureAwait(false);
-                    if (license.Type != MP4Service.Contract.Client.LicenseType.MP4)
-                        throw new WarningMessageException("Invalid MP4 license type");
-
-                    task = await mp4Client.Convert(new MP4Service.Contract.Client.TaskParam
-                    {
-                        LicenseId = licenseKey,
-                        ScoId = long.Parse(recordingId),
-                    }).ConfigureAwait(false);
-                }
-                catch (AggregateException ex)
-                {
-                    return Json(ProcessAggregateException(ex, "Message from MP4 API: "));
-                }
-                catch (ApiException ex)
-                {
-                    return Json(ProcessApiException(ex, "Message from MP4 API: "));
-                }
+//namespace EdugameCloud.Lti.Controllers
+//{
+//    public partial class Mp4ServiceController : BaseController
+//    {
+//        public Mp4ServiceController(
+//            LmsUserSessionModel userSessionModel,
+//            IAdobeConnectAccountService acAccountService,
+//            ApplicationSettingsProvider settings,
+//            ILogger logger)
+//            : base(userSessionModel, acAccountService, settings, logger)
+//        {
+//        }
 
 
-                return Json(OperationResult.Success(task.Status.ToString()));
-            }
-            catch (Exception ex)
-            {
-                string errorMessage = GetOutputErrorMessage("Convert", lmsCompany, ex);
-                return Json(OperationResult.Error(errorMessage));
-            }
-        }
+//        [HttpPost]
+//        public virtual async Task<JsonResult> Convert(string lmsProviderName, string recordingId)
+//        {
+//            LmsCompany lmsCompany = null;
+//            try
+//            {
+//                var session = GetReadOnlySession(lmsProviderName);
+//                lmsCompany = session.LmsCompany;
 
-        [HttpPost]
-        public virtual async Task<JsonResult> ConvertWithSubtitles(string lmsProviderName, string recordingId)
-        {
-            LmsCompany lmsCompany = null;
-            try
-            {
-                var session = GetReadOnlySession(lmsProviderName);
-                lmsCompany = session.LmsCompany;
+//                string licenseKey = lmsCompany.GetSetting<string>(LmsCompanySettingNames.Mp4ServiceLicenseKey);
+//                if (string.IsNullOrWhiteSpace(licenseKey))
+//                    throw new WarningMessageException("Can't find your MP4Service licence. Contact administrator.");
 
-                string licenseKey = lmsCompany.GetSetting<string>(LmsCompanySettingNames.Mp4ServiceWithSubtitlesLicenseKey);
-                if (string.IsNullOrWhiteSpace(licenseKey))
-                    throw new WarningMessageException("Can't find your MP4Service licence. Contact administrator.");
+//                var mp4Client = IoC.Resolve<Mp4ServiceTaskClient>();
+//                MP4Service.Contract.Client.DataTask task = null;
+//                try
+//                {
+//                    var license = await mp4Client.GetLicense(new Guid(licenseKey)).ConfigureAwait(false);
+//                    if (license.Type != MP4Service.Contract.Client.LicenseType.MP4)
+//                        throw new WarningMessageException("Invalid MP4 license type");
 
-                var mp4Client = IoC.Resolve<Mp4ServiceTaskClient>();
-                MP4Service.Contract.Client.DataTask task = null;
-                try
-                {
-                    var license = await mp4Client.GetLicense(new Guid(licenseKey)).ConfigureAwait(false);
-                    if (license.Type != MP4Service.Contract.Client.LicenseType.MP4WithSubtitles)
-                        throw new WarningMessageException("Invalid MP4 license type");
-
-                    task = await mp4Client.Convert(new MP4Service.Contract.Client.TaskParam
-                    {
-                        LicenseId = licenseKey,
-                        ScoId = long.Parse(recordingId),
-                    }).ConfigureAwait(false);
-                }
-                catch (AggregateException ex)
-                {
-                    return Json(ProcessAggregateException(ex, "Message from MP4 API: "));
-                }
-                catch (ApiException ex)
-                {
-                    return Json(ProcessApiException(ex, "Message from MP4 API: "));
-                }
-
-                return Json(OperationResult.Success(task.Status.ToString()));
-            }
-            catch (Exception ex)
-            {
-                string errorMessage = GetOutputErrorMessage("ConvertWithSubtitles", lmsCompany, ex);
-                return Json(OperationResult.Error(errorMessage));
-            }
-        }
+//                    task = await mp4Client.Convert(new MP4Service.Contract.Client.TaskParam
+//                    {
+//                        LicenseId = licenseKey,
+//                        ScoId = long.Parse(recordingId),
+//                    }).ConfigureAwait(false);
+//                }
+//                catch (AggregateException ex)
+//                {
+//                    return Json(ProcessAggregateException(ex, "Message from MP4 API: "));
+//                }
+//                catch (ApiException ex)
+//                {
+//                    return Json(ProcessApiException(ex, "Message from MP4 API: "));
+//                }
 
 
-        [HttpPost]
-        public virtual ActionResult AccessFile(string lmsProviderName, string scoId)
-        {
-            LmsCompany lmsCompany = null;
-            try
-            {
-                var session = GetReadOnlySession(lmsProviderName);
-                lmsCompany = session.LmsCompany;
+//                return Json(OperationResultWithData<string>.Success(task.Status.ToString()));
+//            }
+//            catch (Exception ex)
+//            {
+//                string errorMessage = GetOutputErrorMessage("Convert", lmsCompany, ex);
+//                return Json(OperationResult.Error(errorMessage));
+//            }
+//        }
 
-                string url = AccessSco(lmsCompany, session.LtiSession.LtiParam, scoId, this.GetAdobeConnectProvider(lmsCompany));
+//        [HttpPost]
+//        public virtual async Task<JsonResult> ConvertWithSubtitles(string lmsProviderName, string recordingId)
+//        {
+//            LmsCompany lmsCompany = null;
+//            try
+//            {
+//                var session = GetReadOnlySession(lmsProviderName);
+//                lmsCompany = session.LmsCompany;
 
-                //return this.Redirect(url);
-                return Json(OperationResult.Success(url));
-            }
-            catch (WarningMessageException ex)
-            {
-                this.ViewBag.Message = ex.Message;
-                // TRICK: to increase window height
-                this.ViewBag.DebugError = "eSyncTraining Inc.";
-                return this.View("~/Views/Lti/LtiError.cshtml");
-            }
-            catch (Exception ex)
-            {
-                logger.ErrorFormat(ex, "Mp4Video exception. sco-id:{0}. SessionID: {1}.", scoId, lmsProviderName);
-                this.ViewBag.DebugError = IsDebug ? (ex.Message + ex.StackTrace) : string.Empty;
-                return this.View("~/Views/Lti/LtiError.cshtml");
-            }
-        }
+//                string licenseKey = lmsCompany.GetSetting<string>(LmsCompanySettingNames.Mp4ServiceWithSubtitlesLicenseKey);
+//                if (string.IsNullOrWhiteSpace(licenseKey))
+//                    throw new WarningMessageException("Can't find your MP4Service licence. Contact administrator.");
+
+//                var mp4Client = IoC.Resolve<Mp4ServiceTaskClient>();
+//                MP4Service.Contract.Client.DataTask task = null;
+//                try
+//                {
+//                    var license = await mp4Client.GetLicense(new Guid(licenseKey)).ConfigureAwait(false);
+//                    if (license.Type != MP4Service.Contract.Client.LicenseType.MP4WithSubtitles)
+//                        throw new WarningMessageException("Invalid MP4 license type");
+
+//                    task = await mp4Client.Convert(new MP4Service.Contract.Client.TaskParam
+//                    {
+//                        LicenseId = licenseKey,
+//                        ScoId = long.Parse(recordingId),
+//                    }).ConfigureAwait(false);
+//                }
+//                catch (AggregateException ex)
+//                {
+//                    return Json(ProcessAggregateException(ex, "Message from MP4 API: "));
+//                }
+//                catch (ApiException ex)
+//                {
+//                    return Json(ProcessApiException(ex, "Message from MP4 API: "));
+//                }
+
+//                return Json(OperationResultWithData<string>.Success(task.Status.ToString()));
+//            }
+//            catch (Exception ex)
+//            {
+//                string errorMessage = GetOutputErrorMessage("ConvertWithSubtitles", lmsCompany, ex);
+//                return Json(OperationResult.Error(errorMessage));
+//            }
+//        }
 
 
-        private string AccessSco(LmsCompany lmsCompany, LtiParamDTO param, string scoId,
-             IAdobeConnectProxy adobeConnectProvider)
-        {
-            var breezeToken = string.Empty;
+//        [HttpPost]
+//        public virtual ActionResult AccessFile(string lmsProviderName, string scoId)
+//        {
+//            LmsCompany lmsCompany = null;
+//            try
+//            {
+//                var session = GetReadOnlySession(lmsProviderName);
+//                lmsCompany = session.LmsCompany;
 
-            IAdobeConnectProxy provider = adobeConnectProvider;
+//                string url = AccessSco(lmsCompany, session.LtiSession.LtiParam, scoId, this.GetAdobeConnectProvider(lmsCompany));
 
-            LmsUserModel lmsUserModel = IoC.Resolve<LmsUserModel>();
-            var lmsUser = lmsUserModel.GetOneByUserIdAndCompanyLms(param.lms_user_id, lmsCompany.Id).Value;
-            if (lmsUser == null)
-            {
-                throw new WarningMessageException(string.Format("No user with id {0} found in the database.",
-                    param.lms_user_id));
-            }
+//                //return this.Redirect(url);
+//                return Json(OperationResultWithData<string>.Success(url));
+//            }
+//            catch (WarningMessageException ex)
+//            {
+//                this.ViewBag.Message = ex.Message;
+//                // TRICK: to increase window height
+//                this.ViewBag.DebugError = "eSyncTraining Inc.";
+//                return this.View("~/Views/Lti/LtiError.cshtml");
+//            }
+//            catch (Exception ex)
+//            {
+//                logger.ErrorFormat(ex, "Mp4Video exception. sco-id:{0}. SessionID: {1}.", scoId, lmsProviderName);
+//                this.ViewBag.DebugError = IsDebug ? (ex.Message + ex.StackTrace) : string.Empty;
+//                return this.View("~/Views/Lti/LtiError.cshtml");
+//            }
+//        }
 
-            var principalInfo = lmsUser.PrincipalId != null
-                ? provider.GetOneByPrincipalId(lmsUser.PrincipalId).PrincipalInfo
-                : null;
-            var registeredUser = principalInfo != null ? principalInfo.Principal : null;
 
-            if (registeredUser != null)
-            {
-                var meetingSetup = IoC.Resolve<IMeetingSetup>();
-                breezeToken = meetingSetup.ACLogin(lmsCompany, param, lmsUser, registeredUser, provider);
-            }
-            else
-            {
-                throw new WarningMessageException(string.Format(
-                    "No user with principal id {0} found in Adobe Connect.", lmsUser.PrincipalId ?? string.Empty));
-            }
+//        private string AccessSco(LmsCompany lmsCompany, LtiParamDTO param, string scoId,
+//             IAdobeConnectProxy adobeConnectProvider)
+//        {
+//            var breezeToken = string.Empty;
 
-            // check is user already has read permission!!!
-            // TODO: setup only if source recording is accessible??
-            provider.UpdateScoPermissionForPrincipal(scoId, registeredUser.PrincipalId, Esynctraining.AC.Provider.Entities.MeetingPermissionId.view);
+//            IAdobeConnectProxy provider = adobeConnectProvider;
 
-            var baseUrl = lmsCompany.AcServer + provider.GetScoInfo(scoId).ScoInfo.UrlPath;
+//            LmsUserModel lmsUserModel = IoC.Resolve<LmsUserModel>();
+//            var lmsUser = lmsUserModel.GetOneByUserIdAndCompanyLms(param.lms_user_id, lmsCompany.Id).Value;
+//            if (lmsUser == null)
+//            {
+//                throw new WarningMessageException(string.Format("No user with id {0} found in the database.",
+//                    param.lms_user_id));
+//            }
 
-            return GetDownloadLink(lmsCompany.AcServer, provider.GetScoInfo(scoId).ScoInfo.UrlPath.Replace("/", ""), "mp4") + "&session=" + breezeToken;
-        }
+//            var principalInfo = lmsUser.PrincipalId != null
+//                ? provider.GetOneByPrincipalId(lmsUser.PrincipalId).PrincipalInfo
+//                : null;
+//            var registeredUser = principalInfo != null ? principalInfo.Principal : null;
 
-        private string GetDownloadLink(string acServer, string downloadName, string format)
-        {
-            string fileName = downloadName.Substring(0, downloadName.Length - 3) + ".mp4";
+//            if (registeredUser != null)
+//            {
+//                var meetingSetup = IoC.Resolve<IMeetingSetup>();
+//                breezeToken = meetingSetup.ACLogin(lmsCompany, param, lmsUser, registeredUser, provider);
+//            }
+//            else
+//            {
+//                throw new WarningMessageException(string.Format(
+//                    "No user with principal id {0} found in Adobe Connect.", lmsUser.PrincipalId ?? string.Empty));
+//            }
 
-            return string.Format(
-                "{0}/{1}/output/{2}?download={2}",
-                acServer.Replace(@"api/xml", string.Empty).Trim('/'),
-                downloadName,
-                fileName);            
-        }
+//            // check is user already has read permission!!!
+//            // TODO: setup only if source recording is accessible??
+//            provider.UpdateScoPermissionForPrincipal(scoId, registeredUser.PrincipalId, Esynctraining.AC.Provider.Entities.MeetingPermissionId.view);
 
-        protected OperationResult ProcessAggregateException(AggregateException ex, string baseMessage)
-        {
-            foreach (ApiException exception in ex.InnerExceptions.Where(x => x is ApiException))
-            {
-                return ProcessApiException(exception, baseMessage);
-            }
+//            var baseUrl = lmsCompany.AcServer + provider.GetScoInfo(scoId).ScoInfo.UrlPath;
 
-            foreach (Exception exception in ex.InnerExceptions)
-            {
-                return OperationResult.Error(baseMessage + exception.Message);
-            }
+//            return GetDownloadLink(lmsCompany.AcServer, provider.GetScoInfo(scoId).ScoInfo.UrlPath.Replace("/", ""), "mp4") + "&session=" + breezeToken;
+//        }
 
-            return OperationResult.Error(baseMessage + ex.Message);
-        }
+//        private string GetDownloadLink(string acServer, string downloadName, string format)
+//        {
+//            string fileName = downloadName.Substring(0, downloadName.Length - 3) + ".mp4";
 
-        protected OperationResult ProcessApiException(ApiException ex, string baseMessage)
-        {
-            return OperationResult.Error(baseMessage + (ex.ErrorDetails != null ? ex.ErrorDetails.ToString() : ex.Message));
-        }
+//            return string.Format(
+//                "{0}/{1}/output/{2}?download={2}",
+//                acServer.Replace(@"api/xml", string.Empty).Trim('/'),
+//                downloadName,
+//                fileName);            
+//        }
 
-    }
+//        protected OperationResult ProcessAggregateException(AggregateException ex, string baseMessage)
+//        {
+//            foreach (ApiException exception in ex.InnerExceptions.Where(x => x is ApiException))
+//            {
+//                return ProcessApiException(exception, baseMessage);
+//            }
 
-}
+//            foreach (Exception exception in ex.InnerExceptions)
+//            {
+//                return OperationResult.Error(baseMessage + exception.Message);
+//            }
+
+//            return OperationResult.Error(baseMessage + ex.Message);
+//        }
+
+//        protected OperationResult ProcessApiException(ApiException ex, string baseMessage)
+//        {
+//            return OperationResult.Error(baseMessage + (ex.ErrorDetails != null ? ex.ErrorDetails.ToString() : ex.Message));
+//        }
+
+//    }
+
+//}
