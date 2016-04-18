c29bb5c203a921b373fd7ae3e0a5717fea6f8e67
sergey <sergeyi@esynctraining.com>
2016-04-18 12:40:00 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@10660 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ AC/trunk/Esynctraining.AdobeConnect/IMeetingService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Esynctraining.AC.Provider.Entities;
+
+namespace Esynctraining.AdobeConnect
+{
+    public interface IMeetingService
+    {
+        IEnumerable<ScoContent> GetUserMeetings();
+
+        IEnumerable<ScoContent> GetSharedMeetings();
+
+    }
+
+}
---
+++ AC/trunk/Esynctraining.AdobeConnect/MeetingService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Esynctraining.AC.Provider.DataObjects.Results;
+using Esynctraining.AC.Provider.Entities;
+using Esynctraining.Core.Logging;
+
+namespace Esynctraining.AdobeConnect
+{
+    public class MeetingService : IMeetingService
+    {
+        private readonly ILogger _logger;
+        private readonly IAdobeConnectAccountService _acAccountService;
+        private readonly IAdobeConnectProxy _provider;
+
+
+        public MeetingService(ILogger logger, IAdobeConnectAccountService acAccountService, IAdobeConnectAccess2 creds)
+        {
+            _logger = logger;
+            _acAccountService = acAccountService;
+
+            _provider = _acAccountService.GetProvider2(creds);
+        }
+
+
+        public IEnumerable<ScoContent> GetUserMeetings()
+        {
+            var shortcut = _provider.GetShortcutByType("my-meetings");
+
+            if (shortcut == null)
+                throw new WarningMessageException("User is not Meeting Host");
+
+            ScoContentCollectionResult result = _provider.GetContentsByScoId(shortcut.ScoId);
+
+            return result.Values;
+        }
+        
+        public IEnumerable<ScoContent> GetSharedMeetings()
+        {
+            var shortcut = _provider.GetShortcutByType("meetings");
+
+            ScoContentCollectionResult result = _provider.GetContentsByScoId(shortcut.ScoId);
+
+            return result.Values;
+        }
+
+    }
+
+}
