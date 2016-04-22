dd9303a8b370f5cb1505890e5b23456a66cbf72d
sergey <sergeyi@esynctraining.com>
2016-04-22 14:53:45 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@10713 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- Core/trunk/Esynctraining.WebApi.Client/HttpResponseMessageExtentions.cs
+++ Core/trunk/Esynctraining.WebApi.Client/HttpResponseMessageExtentions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using Newtonsoft.Json;
@@ -9,45 +10,53 @@ namespace Esynctraining.WebApi.Client
     {
         public static ApiException CreateApiException(this HttpResponseMessage response)
         {
+            if (response == null)
+                throw new ArgumentNullException("response");
+
             var httpErrorObject = response.Content.ReadAsStringAsync().Result;
-            
-            ApiError deserializedErrorObject =
-                JsonConvert.DeserializeObject<ApiError>(httpErrorObject);
-
-            // Now wrap into an exception which best fullfills the needs of your application:
-            //ApiException ex = (deserializedErrorObject != null)
-            //    ? new ApiException(deserializedErrorObject.ToString(), response, deserializedErrorObject)
-            //    : new ApiException(response, deserializedErrorObject);
-
-            string responseUri = response.RequestMessage.RequestUri.ToString();
-            ApiException ex = (deserializedErrorObject != null)
-                ? new ApiException(responseUri, response, deserializedErrorObject)
-                : new ApiException(responseUri, response, deserializedErrorObject);
-
-            // Sometimes, there may be Model Errors:
-            if (deserializedErrorObject.modelState != null)
+            try
             {
-                var errors =
-                    deserializedErrorObject.modelState
-                                            .Select(kvp => string.Join(". ", kvp.Value));
-                for (int i = 0; i < errors.Count(); i++)
+                ApiError deserializedErrorObject =
+                    JsonConvert.DeserializeObject<ApiError>(httpErrorObject);
+
+                // Now wrap into an exception which best fullfills the needs of your application:
+                //ApiException ex = (deserializedErrorObject != null)
+                //    ? new ApiException(deserializedErrorObject.ToString(), response, deserializedErrorObject)
+                //    : new ApiException(response, deserializedErrorObject);
+
+                string responseUri = response.RequestMessage.RequestUri.ToString();
+                ApiException ex = (deserializedErrorObject != null)
+                    ? new ApiException(responseUri, response, deserializedErrorObject)
+                    : new ApiException(responseUri, response, deserializedErrorObject);
+
+                // Sometimes, there may be Model Errors:
+                if (deserializedErrorObject.modelState != null)
                 {
-                    // Wrap the errors up into the base Exception.Data Dictionary:
-                    ex.Data.Add(i, errors.ElementAt(i));
+                    var errors = deserializedErrorObject.modelState
+                        .Select(kvp => string.Join(". ", kvp.Value));
+                    for (int i = 0; i < errors.Count(); i++)
+                    {
+                        // Wrap the errors up into the base Exception.Data Dictionary:
+                        ex.Data.Add(i, errors.ElementAt(i));
+                    }
                 }
+                // Othertimes, there may not be Model Errors:
+                else
+                {
+                    var error =
+                        JsonConvert.DeserializeObject<Dictionary<string, string>>(httpErrorObject);
+                    foreach (var kvp in error)
+                    {
+                        // Wrap the errors up into the base Exception.Data Dictionary:
+                        ex.Data.Add(kvp.Key, kvp.Value);
+                    }
+                }
+                return ex;
             }
-            // Othertimes, there may not be Model Errors:
-            else
+            catch (Exception ex)
             {
-                var error =
-                    JsonConvert.DeserializeObject<Dictionary<string, string>>(httpErrorObject);
-                foreach (var kvp in error)
-                {
-                    // Wrap the errors up into the base Exception.Data Dictionary:
-                    ex.Data.Add(kvp.Key, kvp.Value);
-                }
+                throw new InvalidOperationException(string.Format("Can't parse {0} to exception.", httpErrorObject), ex);
             }
-            return ex;
         }
 
     }
