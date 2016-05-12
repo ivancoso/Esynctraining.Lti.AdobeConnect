4c7520588e086b0c2533dbadfe2f5f4b801b339c
sergey <sergeyi@esynctraining.com>
2016-05-12 10:30:12 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@11004 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ Core/trunk/Esynctraining.Wcf.ErrorHandling/ErrorHandlerBehavior.cs
@@ -0,0 +1,21 @@
+﻿namespace Esynctraining.Wcf.ErrorHandling
+{
+    using System;
+    using System.ServiceModel.Configuration;
+
+    public sealed class ErrorHandlerBehavior : BehaviorExtensionElement
+    {
+        public override Type BehaviorType
+        {
+            get { return typeof(ErrorServiceBehavior); }
+        }
+
+
+        protected override object CreateBehavior()
+        {
+            return new ErrorServiceBehavior();
+        }
+
+    }
+
+}
---
+++ Core/trunk/Esynctraining.Wcf.ErrorHandling/ErrorServiceBehavior.cs
@@ -0,0 +1,35 @@
+﻿namespace Esynctraining.Wcf.ErrorHandling
+{
+    using System.Collections.ObjectModel;
+    using System.ServiceModel;
+    using System.ServiceModel.Channels;
+    using System.ServiceModel.Description;
+    using System.ServiceModel.Dispatcher;
+
+    internal sealed class ErrorServiceBehavior : IServiceBehavior
+    {
+        public void AddBindingParameters(
+            ServiceDescription serviceDescription,
+            ServiceHostBase serviceHostBase,
+            Collection<ServiceEndpoint> endpoints,
+            BindingParameterCollection bindingParameters)
+        {
+        }
+
+        public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+            var handler = new Log4NetErrorHandler();
+
+            foreach (ChannelDispatcher dispatcher in serviceHostBase.ChannelDispatchers)
+            {
+                dispatcher.ErrorHandlers.Add(handler);
+            }
+        }
+
+        public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+        }
+
+    }
+
+}
