431f3a54a6f0de5321bbc79fa46b9cd487302573
igor <igor@esynctraining.com>
2016-06-10 09:10:02 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@11387 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ Core/trunk/Esynctraining.Core.FullText/Esynctraining.Core.FullText/Esynctraining.Core.FullText/FullTextEnabledAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Esynctraining.Core.Business.Models
+{
+    using System;
+
+    /// <summary>
+    /// The full text enabled attribute.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class FullTextEnabledAttribute : Attribute
+    {
+    }
+}
