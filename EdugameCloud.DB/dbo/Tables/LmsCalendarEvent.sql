299845991b358ea78fe1a020f893406ad92dfa19
ivan <ivan@esynctraining.com>
2016-06-23 15:31:02 +0000

    DB update for Sakai Calendar events
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@11617 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.DB/dbo/Tables/LmsCalendarEvent.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[LmsCalendarEvent](
+	[lmsCalendarEventId] [int] IDENTITY(1,1) NOT NULL,
+	[eventId] [nvarchar](50) NOT NULL,
+	[name] [nvarchar](200) NOT NULL,
+	[startDate] [datetime2](7) NOT NULL,
+	[endDate] [datetime2](7) NOT NULL,
+	[lmsCourseMeetingId] [int] NOT NULL,
+CONSTRAINT [PK_LmsCalendarEvent] PRIMARY KEY CLUSTERED ([lmsCalendarEventId] ASC),
+CONSTRAINT [FK_LmsCalendarEvent_LmsCourseMeeting] FOREIGN KEY([lmsCourseMeetingId]) REFERENCES [dbo].[LmsCourseMeeting] ([lmsCourseMeetingId])
+);
