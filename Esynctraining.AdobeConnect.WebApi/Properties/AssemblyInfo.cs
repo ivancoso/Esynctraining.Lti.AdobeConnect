182765959a0de168ba75fdc49edccf8b54468bcf
sergey <sergeyi@esynctraining.com>
2016-07-01 12:41:26 +0000

    *v. 1.7.0.0*
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@11716 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- Esynctraining.AdobeConnect.WebApi/Properties/AssemblyInfo.cs
+++ Esynctraining.AdobeConnect.WebApi/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Esynctraining.AdobeConnect.WebApi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("eSyncTraining Inc.")]
 [assembly: AssemblyProduct("Esynctraining.AdobeConnect.WebApi")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © eSyncTraining Inc. 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
