e2b9f35b27c24b5540c30e2649a8b3041048fb49
Anton_a <Anton_a@esynctraining.com>
2016-09-05 13:49:35 +0000

    test improved
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@12576 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- AC/trunk/Esynctraining.AdobeConnect.Tests/Mp4Tests.cs
+++ AC/trunk/Esynctraining.AdobeConnect.Tests/Mp4Tests.cs
@@ -7,6 +7,7 @@ using Esynctraining.Core.Logging;
 using Esynctraining.Core.Utils;
 using Esynctraining.Windsor;
 using log4net.Config;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace Esynctraining.AdobeConnect.Tests
@@ -26,8 +27,15 @@ namespace Esynctraining.AdobeConnect.Tests
             var proxy = new AdobeConnectProxy(new AdobeConnectProvider(new ConnectionDetails(serviceUrl)), logger, serviceUrl, string.Empty  );
             var loginResult = proxy.Login(new UserCredentials("anton@esynctraining.com", "NEp2NV44Sj"));
             var getFieldValue = proxy.GetAclField("44636", "x-385564");
-
-
+            
+            var mp4 = JsonConvert.DeserializeObject<Mp4Dto>(getFieldValue);
         }
+    }
+
+
+    public class Mp4Dto
+    {
+        public string mp4 { get; set; }
+        public string vtt { get; set; }
     }
 }
