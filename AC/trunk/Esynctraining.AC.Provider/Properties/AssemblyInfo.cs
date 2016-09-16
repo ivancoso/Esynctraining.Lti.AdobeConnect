45583828d3646e72704f8eaa19433930fbc524cd
sergey <sergeyi@esynctraining.com>
2016-09-16 14:20:36 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@12720 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- AC/trunk/Esynctraining.AC.Provider/Properties/AssemblyInfo.cs
+++ AC/trunk/Esynctraining.AC.Provider/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.78")]
-[assembly: AssemblyFileVersion("1.0.1.78")]
+[assembly: AssemblyVersion("1.0.1.79")]
+[assembly: AssemblyFileVersion("1.0.1.79")]
 //[assembly: AssemblyInformationalVersion("1.0.1.48-alpha")]
 
