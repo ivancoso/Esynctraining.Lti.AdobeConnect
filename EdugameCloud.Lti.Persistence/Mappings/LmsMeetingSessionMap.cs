012353b224ca32b6d62169c6b7e0de7d549084db
ivan <ivan@esynctraining.com>
2016-10-17 07:39:16 +0000

    Meeting sessions feature - mapping for eventId(nullable)
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@12974 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.Lti.Persistence/Mappings/LmsMeetingSessionMap.cs
+++ EdugameCloud.Lti.Persistence/Mappings/LmsMeetingSessionMap.cs
@@ -9,7 +9,7 @@ namespace EdugameCloud.Lti.Persistence.Mappings
         {
             this.Table("LmsCalendarEvent");
             this.Id(x => x.Id).Column("lmsCalendarEventId");
-            this.Map(x => x.EventId).Not.Nullable().Length(50);
+            this.Map(x => x.EventId).Nullable().Length(50);
             this.Map(x => x.Name).Not.Nullable().Length(200);
             this.Map(x => x.StartDate).Not.Nullable();
             this.Map(x => x.EndDate).Not.Nullable();
