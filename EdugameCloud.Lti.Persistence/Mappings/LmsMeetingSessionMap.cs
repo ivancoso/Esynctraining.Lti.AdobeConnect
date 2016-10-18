ae270e60992a0471722d0866092a65466fafe5df
ivan <ivan@esynctraining.com>
2016-10-18 06:37:36 +0000

    updated DB table and key names
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@12994 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.Lti.Persistence/Mappings/LmsMeetingSessionMap.cs
+++ EdugameCloud.Lti.Persistence/Mappings/LmsMeetingSessionMap.cs
@@ -7,8 +7,6 @@ namespace EdugameCloud.Lti.Persistence.Mappings
     {
         public LmsMeetingSessionMap()
         {
-            this.Table("LmsCalendarEvent");
-            this.Id(x => x.Id).Column("lmsCalendarEventId");
             this.Map(x => x.EventId).Nullable().Length(50);
             this.Map(x => x.Name).Not.Nullable().Length(200);
             this.Map(x => x.StartDate).Not.Nullable();
