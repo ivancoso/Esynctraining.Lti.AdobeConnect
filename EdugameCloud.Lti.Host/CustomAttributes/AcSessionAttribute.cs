399e7a617591b4f4f6c4ff313fcdb97fd6804a16
Anton_a <Anton_a@esynctraining.com>
2016-10-24 11:45:39 +0000

    ac session att added
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@13040 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.Lti.Host/CustomAttributes/AcSessionAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace EdugameCloud.Lti.Host.CustomAttributes
+{
+    public class AcSessionAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            return true;
+        }
+    }
+}
