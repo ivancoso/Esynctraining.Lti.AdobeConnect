ee91df4b31ba3b70a3084befeb00aaa354fa1dd6
Anton_a <Anton_a@esynctraining.com>
2016-12-13 12:00:08 +0000

    Sakai idempotent check if question types are linked to lms
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@13659 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.DB/Updates/20161210-DbUpdate-1-7-7.sql
@@ -0,0 +1,89 @@
+﻿if not exists (
+    select 1 from QuestionType 
+	where [Type] = N'Multiple Answer'
+)
+begin
+	insert into QuestionType values (N'Multiple Answer', 18, NULL,NULL,NULL,NULL,1, N'ICON_MULTI_CHOICE'  )
+end
+
+
+
+  declare @sakaiId int
+  select @sakaiId = lmsProviderId
+  from LMSProvider
+  where lmsProvider = N'Sakai'
+
+  --print @sakaiId
+
+  if not exists (
+      select 1 from [LmsQuestionType]
+	  where lmsProviderId = @sakaiId and questionTypeId = 2
+	  )
+	  begin
+		insert into [LmsQuestionType] values(@sakaiId, 2, N'True/False', NULL)
+	  end
+	  else
+	  begin
+		print 'already in db'
+	  end
+
+
+
+
+	    if not exists (
+      select 1 from [LmsQuestionType]
+	  where lmsProviderId = @sakaiId and questionTypeId = 17
+	  )
+	  begin
+		insert into [LmsQuestionType] values(@sakaiId, 17, N'Test Numeric', NULL)
+	  end
+	  else
+	  begin
+		print 'already in db'
+	  end
+
+
+
+
+
+	  if not exists (
+      select 1 from [LmsQuestionType]
+	  where lmsProviderId = @sakaiId and questionTypeId = 23
+	  )
+	  begin
+		insert into [LmsQuestionType] values(@sakaiId, 23, N'Multiple Answer', NULL)
+	  end
+	  else
+	  begin
+		print 'already in db'
+	  end
+
+
+
+
+
+	  if not exists (
+      select 1 from [LmsQuestionType]
+	  where lmsProviderId = @sakaiId and questionTypeId = 13
+	  )
+	  begin
+		insert into [LmsQuestionType] values(@sakaiId, 13, N'Rate Scale (Likert)', NULL)
+	  end
+	  else
+	  begin
+		print 'already in db'
+	  end
+
+
+
+	  if not exists (
+      select 1 from [LmsQuestionType]
+	  where lmsProviderId = @sakaiId and questionTypeId = 18
+	  )
+	  begin
+		insert into [LmsQuestionType] values(@sakaiId, 18, N'Calculated', NULL)
+	  end
+	  else
+	  begin
+		print 'already in db'
+	  end
