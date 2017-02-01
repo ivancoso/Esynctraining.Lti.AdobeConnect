f7622c97b03cd9a741a024ad6a0250421f2e42c9
ivan <ivan@esynctraining.com>
2017-02-01 20:10:23 +0000

    Db updates for 1.8.0 patch
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@14060 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.DB/Updates/20170126-DbUpdate-1-8-0.sql
@@ -0,0 +1,38 @@
+USE [EduGameCloud]
+GO
+
+/****** Object:  Table [dbo].[QuizQuestionResultAnswer]    Script Date: 1/23/2017 10:29:23 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[QuizQuestionResultAnswer](
+	[quizQuestionResultAnswerId] [int] IDENTITY(1,1) NOT NULL,
+	[quizQuestionResultId] [int] NOT NULL,
+	[value] [nvarchar](500) NOT NULL,
+	[quizDistractorAnswerId] [int] NULL,
+ CONSTRAINT [PK_QuizQuestionResultAnswer] PRIMARY KEY CLUSTERED 
+(
+	[quizQuestionResultAnswerId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 80) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[QuizQuestionResultAnswer]  WITH CHECK ADD  CONSTRAINT [FK_QuizQuestionResultAnswer_QuizQuestionResult] FOREIGN KEY([quizQuestionResultId])
+REFERENCES [dbo].[QuizQuestionResult] ([quizQuestionResultId])
+GO
+
+ALTER TABLE [dbo].[QuizQuestionResultAnswer] CHECK CONSTRAINT [FK_QuizQuestionResultAnswer_QuizQuestionResult]
+GO
+
+ALTER TABLE [dbo].[QuizQuestionResultAnswer]  WITH CHECK ADD  CONSTRAINT [FK_QuizQuestionResultAnswer_DistractorAnswer] FOREIGN KEY([quizDistractorAnswerId])
+REFERENCES [dbo].[Distractor] ([distractorId])
+GO
+
+ALTER TABLE [dbo].[QuizQuestionResultAnswer] CHECK CONSTRAINT [FK_QuizQuestionResultAnswer_DistractorAnswer]
+GO
+
+
---
+++ EdugameCloud.DB/dbo/Tables/QuizQuestionResultAnswer.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[QuizQuestionResultAnswer] (
+    [quizQuestionResultAnswerId] INT            IDENTITY (1, 1) NOT NULL,
+    [quizQuestionResultId]       INT            NOT NULL,
+    [value]                      NVARCHAR (500) NOT NULL,
+    [quizDistractorAnswerId]     INT            NULL,
+    CONSTRAINT [PK_QuizQuestionResultAnswer] PRIMARY KEY CLUSTERED ([quizQuestionResultAnswerId] ASC) WITH (FILLFACTOR = 80),
+    CONSTRAINT [FK_QuizQuestionResultAnswer_DistractorAnswer] FOREIGN KEY ([quizDistractorAnswerId]) REFERENCES [dbo].[Distractor] ([distractorId]),
+    CONSTRAINT [FK_QuizQuestionResultAnswer_QuizQuestionResult] FOREIGN KEY ([quizQuestionResultId]) REFERENCES [dbo].[QuizQuestionResult] ([quizQuestionResultId])
+);
+
