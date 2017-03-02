efef45527a375950adbab74c1b650d79a8513c30
Anton_a <Anton_a@esynctraining.com>
2017-03-02 07:50:04 +0000

    rest offline quiz
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@14379 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.WCFService/Contracts/IOfflineQuizService.cs
+++ EdugameCloud.WCFService/Contracts/IOfflineQuizService.cs
@@ -1,4 +1,5 @@
 ﻿using System.ServiceModel;
+using System.ServiceModel.Web;
 using EdugameCloud.Core.Domain.DTO.OfflineQuiz;
 using Esynctraining.Core.Domain.Entities;
 
@@ -9,8 +10,11 @@ namespace EdugameCloud.WCFService.Contracts
     {
         [OperationContract]
         [FaultContract(typeof(Error))]
+        [WebGet(UriTemplate = "Quiz/{key}", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
         OfflineQuizDTO GetQuizByKey(string key);
 
+        [OperationContract]
+        [FaultContract(typeof(Error))]
         OfflineQuizResultDTO SendAnswers(OfflineQuizAnswerDTO[] answers);
     }
 }
--- EdugameCloud.WCFService/OfflineQuizService.svc.cs
+++ EdugameCloud.WCFService/OfflineQuizService.svc.cs
@@ -22,10 +22,10 @@ using Esynctraining.Core.Utils;
 
 namespace EdugameCloud.WCFService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CompanyAcDomainsService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select CompanyAcDomainsService.svc or CompanyAcDomainsService.svc.cs at the Solution Explorer and start debugging.
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "OfflineQuizService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select OfflineQuizService.svc or CompanyAcDomainsService.svc.cs at the Solution Explorer and start debugging.
     [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
-    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.PerSession,
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.PerCall,
         IncludeExceptionDetailInFaults = true)]
     public class OfflineQuizService : BaseService, IOfflineQuizService
     {
