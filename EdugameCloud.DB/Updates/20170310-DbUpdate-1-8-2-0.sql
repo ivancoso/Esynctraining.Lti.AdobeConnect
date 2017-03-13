3086d49878b3d710f24c55a6c65e98a1c10f1e26
Anton_a <Anton_a@esynctraining.com>
2017-03-13 14:56:33 +0000

    db adjusted
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@14512 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.DB/Updates/20170310-DbUpdate-1-8-2-0.sql
+++ EdugameCloud.DB/Updates/20170310-DbUpdate-1-8-2-0.sql
@@ -13,13 +13,13 @@ BEGIN
 
 select 
   lang.[language], ac.acSessionId, qr.EventQuizMappingId as eventQuizMappingId, 
-  (select Count(Q.questionid) from Question Q where Q.subModuleItemId=ac.subModuleItemId and q.isActive = 1) as TotalQuestion,
+  (select Count(Q.questionid) from Question Q where Q.subModuleItemId=ac.subModuleItemId and q.isActive = 1 ) as TotalQuestion,
   q.submoduleItemId as subModuleItemId, ac.dateCreated, ac.includeACEmails as includeAcEmails, cat.categoryName, um.acUserModeId, 
    COUNT(QR.quizResultId) AS totalParticipants, 
 	   (SELECT COUNT(QR.quizResultId)
-       FROM QuizResult QR
-       WHERE QR.score > 0 AND ac.acSessionId = QR.acSessionId) AS activeParticipants,
-       (SELECT SUM(score)from QuizResult where acSessionId = ac.acSessionId ) AS TotalScore,          
+       FROM QuizResult QR JOIN Quiz on Quiz.QuizId = QR.QuizId 
+       WHERE QR.score > 0 AND ac.acSessionId = QR.acSessionId and Quiz.isPostQuiz = q.isPostQuiz) AS activeParticipants,
+       (SELECT SUM(score)from QuizResult JOIN Quiz on Quiz.QuizId = QuizResult.QuizId where acSessionId = ac.acSessionId and Quiz.isPostQuiz = q.isPostQuiz ) AS TotalScore,          
 	   --q.isPostQuiz,
 	   q.quizName,
     ac.userId
@@ -29,6 +29,38 @@ where qr.acSessionId= ac.acSessionId and qr.quizId = q.quizId and ac.userId = @u
 and cat.submoduleCategoryId = smi.submoduleCategoryId and ac.languageId = lang.languageId and um.acuserModeId = ac.acuserModeId
 and smi.submoduleItemId = q.submoduleItemId
 and ( (smi.submoduleItemId = ac.submoduleItemId) or q.isPostQuiz = 1)
-group by lang.[language], QR.EventQuizMappingId, q.submoduleItemId, ac.submoduleItemId, ac.dateCreated, ac.includeACEmails, q.quizName, q.quizId, cat.categoryName, um.acUserModeId, ac.userId, ac.acSessionId--, q.isPostQuiz
+group by lang.[language], QR.EventQuizMappingId, q.submoduleItemId, ac.submoduleItemId, ac.dateCreated, ac.includeACEmails, q.quizName, q.quizId, cat.categoryName, um.acUserModeId, ac.userId, ac.acSessionId, q.isPostQuiz
+END
+
+GO
+
+
+ALTER PROCEDURE [dbo].[getQuizResultByACSessionId]  
+	@acSessionId int = null,@subModuleItemID int = null
+AS
+BEGIN
+select sub.quizResultId, sub.participantName, sub.acEmail, sub.score, sub.TotalQuestion, sub.startTime, sub.endTime, 
+		 ROW_NUMBER() OVER (ORDER BY sub.score desc, sub.dateDifference asc) AS position, sub.isCompleted, sub.appMaximizedTime, sub.appInFocusTime, sub.passingScore from (
+SELECT   QR.quizResultId,
+		 QR.participantName,	
+		 QR.acEmail,	 
+		 QR.score,
+		 (select Count(Q.questionid) from Question Q where Q.subModuleItemId=@subModuleItemID) as TotalQuestion,
+	 	 QR.startTime,
+		 QR.endTime,
+		 DATEDIFF(second, QR.startTime, QR.endTime) as dateDifference,
+		 QR.isCompleted,
+		 QR.appMaximizedTime as appMaximizedTime,
+		 QR.appInFocusTime as appInFocusTime,
+		 Q.passingScore as passingScore
+		 
+		    
+FROM     Quiz Q INNER JOIN
+         QuizResult QR ON Q.quizId = QR.quizId
+
+
+WHERE    QR.acSessionId = @acSessionId and Q.submoduleItemId = @subModuleItemID
+) as sub
+
 
 END
