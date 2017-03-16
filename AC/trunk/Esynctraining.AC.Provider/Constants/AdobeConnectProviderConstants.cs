f879e656c063c82ceabf0ea20596fd81f706f31f
sergey <sergeyi@esynctraining.com>
2017-03-16 13:18:07 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@14586 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- AC/trunk/Esynctraining.AC.Provider/Constants/AdobeConnectProviderConstants.cs
+++ AC/trunk/Esynctraining.AC.Provider/Constants/AdobeConnectProviderConstants.cs
@@ -15,14 +15,6 @@
         /// </summary>
         public const int DefaultEventMaxParticipants = 100;
 
-        // not longer used
-        //public const string ConfigStringServiceUrl = "AdobeConnectProvider_ServiceUrl";
-        //public const string ConfigStringEventMaxParticipants = "AdobeConnectProvider_EventMaxParticipants";
-        //public const string ConfigStringProxyUrl = "AdobeConnectProvider_ProxyUrl";
-        //public const string ConfigStringProxyDomain = "AdobeConnectProvider_ProxyDomain";
-        //public const string ConfigStringProxyLogin = "AdobeConnectProvider_ProxyLogin";
-        //public const string ConfigStringProxyPassword = "AdobeConnectProvider_ProxyPassword";
-
         /// <summary>
         /// The date format.
         /// </summary>
@@ -31,12 +23,13 @@
         /// <summary>
         /// The http client timeout for http requests
         /// </summary>
-        public const int DefaultHttpRequestTimeout = 2000*60; //2 min
-        
-		/// <summary>
+        public const int DefaultHttpRequestTimeout = 2000 * 60; //2 min
+
+        /// <summary>
         /// The http client timeout for content download/upload requests
         /// </summary>
-        public const int DefaultHttpContentRequestTimeout = 10000*60; //10 min
+        public const int DefaultHttpContentRequestTimeout = 10000 * 60; //10 min
+
 
         /// <summary>
         /// The maximum number of returned objects for api calls
