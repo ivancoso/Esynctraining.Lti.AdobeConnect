85576416f5be96bea7e1a1903cb09e574ada5d06
sergey <sergeyi@esynctraining.com>
2017-03-23 13:44:22 +0000

    filters wip
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@14725 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.Lti.Api/Filters/CheckModelForNullAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EdugameCloud.Lti.Api.Filters
+{
+    //[AttributeUsage(AttributeTargets.Method, Inherited = true)]
+    public class CheckModelForNullAttribute : ActionFilterAttribute
+    {
+        private readonly Func<IDictionary<string, object>, bool> _validate;
+
+
+        public CheckModelForNullAttribute()
+            : this(arguments => arguments.Any(arg => arg.Value == null))
+        { }
+
+
+        public CheckModelForNullAttribute(Func<IDictionary<string, object>, bool> checkCondition)
+        {
+            _validate = checkCondition;
+        }
+
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (_validate(context.ActionArguments))
+            {
+                context.Result = new BadRequestResult();
+            }
+            base.OnActionExecuting(context);
+        }
+
+    }
+
+}
---
+++ EdugameCloud.Lti.Api/Filters/ValidateModelAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EdugameCloud.Lti.Api.Filters
+{
+    //http://www.jerriepelser.com/blog/validation-response-aspnet-core-webapi/
+    public class ValidateModelAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                // TODO: add to log
+
+                context.Result = new JsonResult(context.ModelState);
+                //actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.BadRequest, actionContext.ModelState);
+            }
+
+        }
+
+    }
+
+}
