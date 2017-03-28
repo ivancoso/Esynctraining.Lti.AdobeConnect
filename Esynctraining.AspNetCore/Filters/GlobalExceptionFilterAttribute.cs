ffbc37295dfbe6eeab47a12384daa366dd8642fa
sergey <sergeyi@esynctraining.com>
2017-03-28 08:25:00 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@14804 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ Esynctraining.AspNetCore/Filters/GlobalExceptionFilterAttribute.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Esynctraining.Core;
+using Esynctraining.Core.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace Esynctraining.AspNetCore.Filters
+{
+    // https://blog.kloud.com.au/2016/03/23/aspnet-core-tips-and-tricks-global-exception-handling/
+    // https://weblog.west-wind.com/posts/2016/oct/16/error-handling-and-exceptionfilter-dependency-injection-for-aspnet-core-apis
+    public class GlobalExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        private readonly ILogger _logger;
+        private readonly bool _isDevelopment;
+
+
+        public GlobalExceptionFilterAttribute(ILoggerFactory loggerFactory, bool isDevelopment)
+        {
+            if (loggerFactory == null)
+                throw new ArgumentNullException(nameof(loggerFactory));
+
+            _logger = loggerFactory.CreateLogger("GlobalExceptionFilter");
+            _isDevelopment = isDevelopment;
+        }
+
+
+        public override void OnException(ExceptionContext context)
+        {
+            string message = string.Empty;
+            var userMessage = context.Exception as IUserMessageException;
+
+            if (userMessage != null)
+            {
+                message = context.Exception.Message;
+            }
+            else
+            {
+                _logger.LogError("GlobalExceptionFilter. {0}.", context.Exception);
+
+                message = _isDevelopment
+                    ? Resources.Messages.ExceptionOccured + context.Exception.ToString()
+                    : Resources.Messages.ExceptionMessage;
+            }
+
+            context.Result = new ObjectResult(OperationResult.Error(message));
+            //context.ExceptionHandled = true;
+        }
+
+    }
+}
---
+++ Esynctraining.AspNetCore/Formatters/JilOutputFormatter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace Esynctraining.AspNetCore.Formatters
+{
+    // https://www.codefluff.com/write-your-own-asp-net-core-mvc-formatters/
+    public class JilOutputFormatter : IOutputFormatter
+    {
+        private static readonly string ContentType = "application/json";
+
+
+        public bool CanWriteResult(OutputFormatterCanWriteContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            return context.ContentType == null || context.ContentType.ToString() == ContentType;
+        }
+
+        public async Task WriteAsync(OutputFormatterWriteContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            var response = context.HttpContext.Response;
+            response.ContentType = ContentType;
+
+            using (var writer = context.WriterFactory(response.Body, Encoding.UTF8))
+            {
+                Jil.JSON.Serialize(context.Object, writer, JilSerializer.JilOptions);
+                await writer.FlushAsync();
+            }
+        }
+
+    }
+
+}
