94dab53c7d9e7107e084c6cafef6d28e54199603
sergey <sergeyi@esynctraining.com>
2017-04-04 12:16:20 +0000

    extract swagger
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@14930 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.Lti.Api.Host/Startup.cs
+++ EdugameCloud.Lti.Api.Host/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Specialized;
-using System.Threading.Tasks;
 using Castle.Core.Resource;
 using Castle.MicroKernel.Lifestyle;
 using Castle.MicroKernel.Registration;
@@ -12,7 +11,6 @@ using Esynctraining.AspNetCore;
 using Esynctraining.AspNetCore.Filters;
 using Esynctraining.AspNetCore.Formatters;
 using Esynctraining.CastleLog4Net;
-using Esynctraining.Core.Extensions;
 using Esynctraining.Core.Providers;
 using Esynctraining.Windsor;
 using FluentValidation;
@@ -21,7 +19,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Swashbuckle.AspNetCore.Swagger;
 
 namespace EdugameCloud.Lti.Api.Host
 {
@@ -73,16 +70,6 @@ namespace EdugameCloud.Lti.Api.Host
                 .AddApiExplorer()
                 .AddDataAnnotations();
 
-            //.AddJsonOptions(options =>
-            // {
-            //     var settings = options.SerializerSettings;
-            //     settings.Formatting = Formatting.Indented;
-            //     settings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
-            //     settings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
-            //     settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-            //     settings.NullValueHandling = NullValueHandling.Ignore;
-            // });
-
             services
                 .AddCors()
                 .AddMemoryCache();
@@ -110,61 +97,8 @@ namespace EdugameCloud.Lti.Api.Host
             container.Install(new EdugameCloud.Core.Logging.LoggerWindsorInstaller());
 
             RegisterLtiComponents(container);
-
             
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "eSyncTraining LTI API", Version = "v1" });
-                //c.DescribeAllParametersInCamelCase();
-                c.IgnoreObsoleteActions();
-                c.IgnoreObsoleteProperties();
-
-                c.MapType<DateTime?>(() =>
-                    new Schema
-                    {
-                        Type = "integer",
-                        Format = "int64",
-                        Example = (long)DateTime.Now.ConvertToUnixTimestamp(),
-                        Description = "MillisecondsSinceUnixEpoch",
-                        //Default = (long)DateTime.Now.ConvertToUnixTimestamp(),
-                        //Pattern = "pattern",
-                    }
-                );
-
-                c.MapType<DateTime>(() =>
-                    new Schema
-                    {
-                        Type = "integer",
-                        Format = "int64",
-                        Example = (long)DateTime.Now.ConvertToUnixTimestamp(),
-                        Description = "MillisecondsSinceUnixEpoch",
-                        //Default = (long)DateTime.Now.ConvertToUnixTimestamp(),
-                        //Pattern = "pattern",
-                    }
-                );
-                
-                if (!HostingEnvironment.IsDevelopment())
-                    c.DocumentFilter<HideNonApiFilter>();
-
-                c.SchemaFilter<ApplyCustomSchemaFilters>();
-
-                c.IncludeXmlComments(string.Format(@"{0}\EdugameCloud.Lti.Api.xml", HostingEnvironment.WebRootPath));
-                c.IncludeXmlComments(string.Format(@"{0}\EdugameCloud.Lti.Core.xml", HostingEnvironment.WebRootPath));
-                c.IncludeXmlComments(string.Format(@"{0}\Esynctraining.AdobeConnect.Api.xml", HostingEnvironment.WebRootPath));
-                c.IncludeXmlComments(string.Format(@"{0}\EdugameCloud.Lti.xml", HostingEnvironment.WebRootPath));
-                
-                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
-                {
-                    Description = "Authorization header using the Bearer scheme. " +
-                    "<br/> Example: \"Authorization: ltiapi {consumerKey};{courseId}\""
-                    + "<br/> {courseId} - LMS course Id."
-                    + "For Sakai {courseId} - is GetHashCode() result for course context_id value.",
-                    Name = "Authorization",
-                    In = "header",
-                    Type = "apiKey"
-                });
-
-            });
+            services.AddLtiSwagger(HostingEnvironment);
 
             //services.AddOptions();
 
@@ -180,37 +114,11 @@ namespace EdugameCloud.Lti.Api.Host
                 .AddFile(Configuration.GetSection("Logging"))
                 .AddFile(Configuration.GetSection("Logging_Serilog_Errors"));
 
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            // https://docs.microsoft.com/en-us/aspnet/core/security/cors
-            //app.Use(next =>
-            //{
-            //    return ctx =>
-            //    {
-            //        // NOTE: skip Swagger related requests
-            //        if (ctx.Request.Method == "OPTIONS" || ctx.Request.Method == "POST")
-            //        {
-            //            // TODO: GC performances
-            //            var origins = Configuration["AppSettings:CorsOrigin"].Split(new char[] { ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
-            //            ctx.Response.Headers.Add("Access-Control-Allow-Origin", origins);
-            //            ctx.Response.Headers.Add("Access-Control-Allow-Headers", new[] { "Authorization, X-Requested-With, Content-Type, Accept, Origin" });
-            //            ctx.Response.Headers.Add("Access-Control-Allow-Methods", new[] { "POST" });
-            //            ctx.Response.Headers.Add("Access-Control-Max-Age", new[] { "86400" }); // 1day
-
-            //            if (ctx.Request.Method == "OPTIONS")
-            //            {
-            //                return Task.FromResult(0);
-            //            }
-            //        }
-
-            //        return next(ctx);
-            //    };
-            //});
-
+            
             //app.UseExceptionHandler(
             //    builder =>
             //    {
@@ -232,12 +140,9 @@ namespace EdugameCloud.Lti.Api.Host
                 .WithHeaders(new[] { "Authorization, X-Requested-With, Content-Type, Accept, Origin" })
                 .SetPreflightMaxAge(TimeSpan.FromDays(1)));
 
-
             app.UseMvc(cfg => 
             {
             });
-
-            //app.UseCors("ALL");
 
             app.UseSwagger(c => 
             {
