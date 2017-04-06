1763fb789eedc529e3d1fcc5f2a9910deeca9ea6
sergey <sergeyi@esynctraining.com>
2017-04-06 14:50:38 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@14974 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.Lti/Controllers/HashCodeController.cs
@@ -0,0 +1,32 @@
+﻿namespace EdugameCloud.Lti.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+
+    public class HashCodeController : Controller
+    {
+        public HashCodeController() { }
+
+
+        [HttpGet]
+        public JsonResult GetHashCode(string value)
+        {
+            return Json(value.GetHashCode(), JsonRequestBehavior.AllowGet);
+        }
+
+
+        protected override JsonResult Json(object data, string contentType,
+                System.Text.Encoding contentEncoding, JsonRequestBehavior behavior)
+        {
+            return new JsonNetResult
+            {
+                Data = data,
+                ContentType = contentType,
+                ContentEncoding = contentEncoding,
+                JsonRequestBehavior = behavior,
+            };
+        }
+
+    }
+
+}
