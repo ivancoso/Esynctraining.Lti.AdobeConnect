ea3a2480a0e04d549863e0d9824fa927405c5234
kniaziuk <kniaziuk@esynctraining.com>
2017-04-13 07:42:48 +0000

    EBP-46 Create ASP.NET Core Proxy Middleware with custom Http Verb mapping.
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@15056 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ Core/trunk/csharp/Core/trunk/Esynctraining.ForwardingProxyMiddleware/Esynctraining.ForwardingProxyMiddleware/ProxyMiddleware.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace Esynctraining.ForwardingProxyMiddleware
+{
+    public class HttpMethodParam
+    {
+        public string Method { get; set; }
+    }
+
+    public class ProxyMiddleware
+    {
+        private const int DefaultWebSocketBufferSize = 4096;
+
+        private readonly RequestDelegate _next;
+        private readonly HttpClient _httpClient;
+        private readonly ProxyOptions _options;
+
+        private static readonly string[] NotForwardedWebSocketHeaders = new[] { "Connection", "Host", "Upgrade", "Sec-WebSocket-Key", "Sec-WebSocket-Version" };
+
+        public ProxyMiddleware(RequestDelegate next, IOptions<ProxyOptions> options)
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+            _options = options.Value;
+
+            if (string.IsNullOrEmpty(_options.Host))
+            {
+                throw new ArgumentException("Options parameter must specify host.", nameof(options));
+            }
+
+            // Setting default Port and Scheme if not specified
+            if (string.IsNullOrEmpty(_options.Port))
+            {
+                if (string.Equals(_options.Scheme, "https", StringComparison.OrdinalIgnoreCase))
+                {
+                    _options.Port = "443";
+                }
+                else
+                {
+                    _options.Port = "80";
+                }
+
+            }
+
+            if (string.IsNullOrEmpty(_options.Scheme))
+            {
+                _options.Scheme = "http";
+            }
+
+            _httpClient = new HttpClient(new HttpClientHandler());
+        }
+
+        public Task Invoke(HttpContext context) => HandleHttpRequest(context);
+
+        private async Task HandleHttpRequest(HttpContext context)
+        {
+            var requestMessage = new HttpRequestMessage();
+            var requestMethod = context.Request.Method;
+
+            // TODO:
+            if (context.Request.HasFormContentType)
+            {
+                var requestBodyStream = new MemoryStream();
+                var originalRequestBody = context.Request.Body;
+
+                await context.Request.Body.CopyToAsync(requestBodyStream);
+                requestBodyStream.Seek(0, SeekOrigin.Begin);
+                context.Request.Body = requestBodyStream;
+
+                //context.Request.Body = originalRequestBody; ??
+
+                var method = context.Request.Form["method"];
+                if (method != Microsoft.Extensions.Primitives.StringValues.Empty)
+                {
+                    requestMethod = method;
+                }
+
+                // HACK:
+                requestBodyStream.Seek(0, SeekOrigin.Begin);
+                context.Request.Body = requestBodyStream;
+            }
+            else if (context.Request.ContentType == "application/json")
+            {
+                var requestBodyStream = new MemoryStream();
+                var originalRequestBody = context.Request.Body;
+
+                await context.Request.Body.CopyToAsync(requestBodyStream);
+                requestBodyStream.Seek(0, SeekOrigin.Begin);
+                context.Request.Body = requestBodyStream;
+
+                //using (StreamReader readStream = new StreamReader(context.Request.Body))
+                StreamReader readStream = new StreamReader(context.Request.Body);
+                {
+                    var documentContents = readStream.ReadToEnd();
+
+                    var deserializedResult = JsonConvert.DeserializeObject<HttpMethodParam>(documentContents);
+
+                    requestMethod = deserializedResult.Method;
+                }
+
+                // HACK:
+                requestBodyStream.Seek(0, SeekOrigin.Begin);
+                context.Request.Body = requestBodyStream;
+            }
+
+            var httpMethod = new HttpMethod(requestMethod);
+
+            if (!HttpMethods.IsGet(requestMethod) &&
+                !HttpMethods.IsHead(requestMethod) &&
+                !HttpMethods.IsDelete(requestMethod) &&
+                !HttpMethods.IsTrace(requestMethod))
+            {
+                var streamContent = new StreamContent(context.Request.Body);
+                requestMessage.Content = streamContent;
+            }
+
+            // Copy the request headers
+            foreach (var header in context.Request.Headers)
+            {
+                if (!requestMessage.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray()) && requestMessage.Content != null)
+                {
+                    requestMessage.Content?.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
+                }
+            }
+
+            requestMessage.Headers.Host = _options.Host + ":" + _options.Port;
+            var uriString = $"{_options.Scheme}://{_options.Host}:{_options.Port}{context.Request.PathBase}{context.Request.Path}{context.Request.QueryString}";
+            requestMessage.RequestUri = new Uri(uriString);
+
+            requestMessage.Method = httpMethod;
+            using (var responseMessage = await _httpClient.SendAsync(requestMessage, HttpCompletionOption.ResponseHeadersRead, context.RequestAborted))
+            {
+                context.Response.StatusCode = (int)responseMessage.StatusCode;
+                foreach (var header in responseMessage.Headers)
+                {
+                    context.Response.Headers[header.Key] = header.Value.ToArray();
+                }
+
+                foreach (var header in responseMessage.Content.Headers)
+                {
+                    context.Response.Headers[header.Key] = header.Value.ToArray();
+                }
+
+                // SendAsync removes chunking from the response. This removes the header so it doesn't expect a chunked response.
+                context.Response.Headers.Remove("transfer-encoding");
+                await responseMessage.Content.CopyToAsync(context.Response.Body);
+            }
+        }
+    }
+}
