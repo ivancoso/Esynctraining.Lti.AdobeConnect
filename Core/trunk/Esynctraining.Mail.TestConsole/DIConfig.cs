74a722c095f85e0888b2ab67a2fcf89ec192009d
sergey <sergeyi@esynctraining.com>
2017-05-17 13:41:34 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@15263 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ Core/trunk/Esynctraining.Mail.TestConsole/DIConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Specialized;
+using Esynctraining.Mail.Configuration;
+using Esynctraining.Mail.Configuration.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Esynctraining.Mail.TestConsole
+{
+    public static class DIConfig
+    {
+        public static IServiceCollection RegisterComponents()
+        {
+            var services = new ServiceCollection()
+                
+                .AddOptions()
+
+                .Configure<SmtpSettings>(Program.Configuration.GetSection("Smtp"))
+                .AddSingleton<SmtpSettings>((sp) => sp.GetService<IOptions<SmtpSettings>>().Value)
+
+                 .Configure<NotificationsSettings>(Program.Configuration.GetSection("Mailer"))
+                .AddSingleton<INotificationsSettings>((sp) => sp.GetService<IOptions<NotificationsSettings>>().Value)
+
+                //.AddTransient<Application>()
+                ;
+
+            return services;
+        }
+
+    }
+
+}
---
+++ Core/trunk/Esynctraining.Mail.TestConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Esynctraining.Mail.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Esynctraining.Mail.TestConsole
+{
+    public class Program
+    {
+        public static IConfiguration Configuration { get; set; }
+
+
+        public static void Main(string[] args)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+               .AddJsonFile(@"appSettings.json", false);
+            Configuration = builder.Build();
+
+            IServiceCollection services = DIConfig.RegisterComponents();
+            var sp = services.BuildServiceProvider();
+
+            var smtp = sp.GetService<SmtpSettings>();
+            var notifications = sp.GetService<INotificationsSettings>();
+
+            var defaultMail = notifications.RecipientSettings.GetByToken("default-email");
+            var from = notifications.SystemEmails.GetByToken(defaultMail.FromToken);
+
+            var app = sp.GetService<Application>();
+            app.Run();
+
+        }
+        
+    }
+}
