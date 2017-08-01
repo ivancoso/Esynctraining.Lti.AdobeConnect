8348b54e04cf217a0a65bc94efd70afdbad422ec
roman <romanp@esynctraining.com>
2017-08-01 20:04:52 +0000

    BOSS-5 Login should be available for "Administrators" and "Reporting" custom group.
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@15627 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- AC/trunk/Esynctraining.AdobeConnect.Security.Abstractions/PermissionProviders/ConfigurationUserGroupPermissionProvider.cs
+++ AC/trunk/Esynctraining.AdobeConnect.Security.Abstractions/PermissionProviders/ConfigurationUserGroupPermissionProvider.cs
@@ -7,10 +7,35 @@ namespace Esynctraining.AdobeConnect.Security.Abstractions.PermissionProviders
 {
     public class ConfigurationUserGroupPermissionProvider : DefaultUserGroupPermissionProvider
     {
+        private string _groups;
+
+        public ConfigurationUserGroupPermissionProvider(string groups)
+        {
+            _groups = groups;
+        }
+
         public override bool UserHasGroupPermission(AdobeConnectProvider provider, UserInfo user)
         {
             if (UserIsSpecialACGroupParticipant(provider, user, PrincipalType.admins))
                 return true;
+
+            if (string.IsNullOrEmpty(_groups))
+                return false;
+
+            string[] configGroups = _groups.Split(',', ';');
+            var groups = provider.GetGroupsByType(PrincipalType.group);
+
+            if (groups?.Values != null)
+            {
+                var filteredGroups = groups.Values.Where(x => configGroups.Any(tg => tg.Trim() == x.Name));
+                foreach (var groupPrincipal in filteredGroups)
+                {
+                    if (UserIsGroupParticipant(provider, user, groupPrincipal))
+                        return true;
+                }
+            }
+
+            return false;
 
             //if (string.IsNullOrEmpty(ConfigurationManager.AppSettings["MonitoringGroups"]))
             //    return false;
@@ -28,7 +53,7 @@ namespace Esynctraining.AdobeConnect.Security.Abstractions.PermissionProviders
             //    }
             //}
 
-            return false;
+            //return false;
         }
     }
 }
