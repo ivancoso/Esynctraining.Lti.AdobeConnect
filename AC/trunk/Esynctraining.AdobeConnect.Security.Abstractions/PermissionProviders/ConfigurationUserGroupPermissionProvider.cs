45701116fc29c516c2539b31b90c861bb9107784
roman <romanp@esynctraining.com>
2017-08-11 14:33:11 +0000

    BOSS-5 Login should be available for "Administrators" and "Reporting" custom group.
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@15731 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- AC/trunk/Esynctraining.AdobeConnect.Security.Abstractions/PermissionProviders/ConfigurationUserGroupPermissionProvider.cs
+++ AC/trunk/Esynctraining.AdobeConnect.Security.Abstractions/PermissionProviders/ConfigurationUserGroupPermissionProvider.cs
@@ -2,35 +2,55 @@ using System.Configuration;
 using System.Linq;
 using Esynctraining.AC.Provider;
 using Esynctraining.AC.Provider.Entities;
+using System;
+using Esynctraining.AC.Provider.DataObjects;
+using Esynctraining.AC.Provider.DataObjects.Results;
 
 namespace Esynctraining.AdobeConnect.Security.Abstractions.PermissionProviders
 {
     public class ConfigurationUserGroupPermissionProvider : DefaultUserGroupPermissionProvider
     {
         private string _groups;
+        private string _domain;
+        private string _login;
+        private string _password;
 
-        public ConfigurationUserGroupPermissionProvider(string groups)
+
+        public ConfigurationUserGroupPermissionProvider(string groups, string domain, string user, string password)
         {
             _groups = groups;
+            _domain = domain;
+            _login = user;
+            _password = password;
         }
 
         public override bool UserHasGroupPermission(AdobeConnectProvider provider, UserInfo user)
         {
-            if (UserIsSpecialACGroupParticipant(provider, user, PrincipalType.admins))
+            var connectionDetails = new ConnectionDetails(new Uri(_domain));
+            var providerAdm = new AdobeConnectProvider(connectionDetails);
+            providerAdm.Logout();
+
+            LoginResult result = providerAdm.Login(new UserCredentials(_login, _password));
+            if (!result.Success)
+            {
+                return false;
+            }
+
+            if (UserIsSpecialACGroupParticipant(providerAdm, user, PrincipalType.admins))
                 return true;
 
             if (string.IsNullOrEmpty(_groups))
                 return false;
 
             string[] configGroups = _groups.Split(',', ';');
-            var groups = provider.GetGroupsByType(PrincipalType.group);
+            var groups = providerAdm.GetGroupsByType(PrincipalType.group);
 
             if (groups?.Values != null)
             {
                 var filteredGroups = groups.Values.Where(x => configGroups.Any(tg => tg.Trim() == x.Name));
                 foreach (var groupPrincipal in filteredGroups)
                 {
-                    if (UserIsGroupParticipant(provider, user, groupPrincipal))
+                    if (UserIsGroupParticipant(providerAdm, user, groupPrincipal))
                         return true;
                 }
             }
