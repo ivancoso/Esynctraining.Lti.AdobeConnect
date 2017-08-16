fc90db1c7ed639ded265f67f5d253c35887566f4
sergey <sergeyi@esynctraining.com>
2017-08-16 12:11:50 +0000

    cleanup
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@15749 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.WCFService/SurveyService.svc.cs
+++ EdugameCloud.WCFService/SurveyService.svc.cs
@@ -9,7 +9,6 @@ namespace EdugameCloud.WCFService
     using EdugameCloud.Core.Business.Models;
     using EdugameCloud.Core.Domain.DTO;
     using EdugameCloud.Core.Domain.Entities;
-    //using EdugameCloud.Core.RTMP;
     using EdugameCloud.Lti.Core.Business.Models;
     using EdugameCloud.WCFService.Base;
     using EdugameCloud.WCFService.Contracts;
@@ -37,29 +36,11 @@ namespace EdugameCloud.WCFService
 
         #region Public Methods and Operators
 
-        /// <summary>
-        ///     All items test.
-        /// </summary>
-        /// <returns>
-        ///     The <see cref="SurveyDTO" />.
-        /// </returns>
         public SurveyDTO[] GetAll()
         {
             return this.SurveyModel.GetAll().Select(x => new SurveyDTO(x)).ToArray();
         }
 
-        /// <summary>
-        /// The get paged.
-        /// </summary>
-        /// <param name="pageIndex">
-        /// The page index.
-        /// </param>
-        /// <param name="pageSize">
-        /// The page size.
-        /// </param>
-        /// <returns>
-        /// The <see cref="PagedSurveyDTO"/>.
-        /// </returns>
         public PagedSurveyDTO GetPaged(int pageIndex, int pageSize)
         {
             int totalCount;
@@ -70,15 +51,6 @@ namespace EdugameCloud.WCFService
             };
         }
 
-        /// <summary>
-        /// The creation of quiz.
-        /// </summary>
-        /// <param name="dto">
-        /// The user.
-        /// </param>
-        /// <returns>
-        /// The <see cref="SurveyDTO"/>.
-        /// </returns>
         public SurveyDTO Create(SurveySMIWrapperDTO dto)
         {
             ValidationResult validationResult;
@@ -95,15 +67,6 @@ namespace EdugameCloud.WCFService
             throw new FaultException<Error>(error, error.errorMessage);
         }
 
-        /// <summary>
-        /// The save update.
-        /// </summary>
-        /// <param name="surveyDTO">
-        /// The survey.
-        /// </param>
-        /// <returns>
-        /// The <see cref="SurveyDTO"/>.
-        /// </returns>
         public SurveyDTO Save(SurveyDTO surveyDTO)
         {
             ValidationResult validationResult;
@@ -119,16 +82,7 @@ namespace EdugameCloud.WCFService
             this.LogError("Survey.Create", error);
             throw new FaultException<Error>(error, error.errorMessage);
         }
-        
-        /// <summary>
-        /// The get by id.
-        /// </summary>
-        /// <param name="id">
-        /// The id.
-        /// </param>
-        /// <returns>
-        /// The <see cref="SurveyDTO"/>.
-        /// </returns>
+
         public SurveyDTO GetById(int id)
         {
             Survey appletResult;
@@ -164,92 +118,32 @@ namespace EdugameCloud.WCFService
             return new SurveyDTO(appletResult);
         }
 
-        /// <summary>
-        /// The get by SMI id.
-        /// </summary>
-        /// <param name="userId">
-        /// The id.
-        /// </param>
-        /// <param name="showLms">
-        /// The show LMS
-        /// </param>
-        /// <returns>
-        /// The <see cref="SurveyFromStoredProcedureDTO"/>.
-        /// </returns>
         public SurveyFromStoredProcedureDTO[] GetSurveysByUserId(int userId, bool? showLms)
         {
             return this.SurveyModel.GetSurveysByUserId(userId, showLms ?? false).ToArray();
         }
 
-        /// <summary>
-        /// The get LMS quizzes.
-        /// </summary>
-        /// <param name="userId">
-        /// The id.
-        /// </param>
-        /// <param name="lmsUserParametersId">
-        /// The LMS User Parameters Id.
-        /// </param>
-        /// <returns>
-        /// The <see cref="SurveyFromStoredProcedureDTO"/>.
-        /// </returns>
         public SurveyFromStoredProcedureDTO[] GetLmsSurveys(int userId, int lmsUserParametersId)
         {
             var lmsUserParameters = LmsUserParametersModel.GetOneById(lmsUserParametersId).Value;
             return this.SurveyModel.GetLmsSurveys(userId, lmsUserParameters.Course, lmsUserParameters.CompanyLms.Id).ToArray();
         }
 
-        /// <summary>
-        /// The get by user id.
-        /// </summary>
-        /// <param name="userId">
-        /// The id.
-        /// </param>
-        /// <returns>
-        /// The <see cref="SurveyFromStoredProcedureDTO"/>.
-        /// </returns>
         public SurveyFromStoredProcedureDTO[] GetSharedSurveysByUserId(int userId)
         {
             return this.SurveyModel.GetSharedForUserSurveysByUserId(userId).ToArray();
         }
 
-        /// <summary>
-        /// The get quiz categories by user id.
-        /// </summary>
-        /// <param name="userId">
-        /// The user id.
-        /// </param>
-        /// <returns>
-        /// The <see cref="SurveyFromStoredProcedureDTO"/>.
-        /// </returns>
         public SMICategoriesFromStoredProcedureDTO[] GetSurveyCategoriesbyUserId(int userId)
         {
             return this.SurveyModel.GetSurveyCategoriesbyUserId(userId).ToArray();
         }
 
-        /// <summary>
-        /// The get survey sub module items by user id.
-        /// </summary>
-        /// <param name="userId">
-        /// The user id.
-        /// </param>
-        /// <returns>
-        /// The <see cref="SurveyFromStoredProcedureDTO"/>.
-        /// </returns>
         public SubModuleItemDTO[] GetSurveySMItemsByUserId(int userId)
         {
             return this.SubModuleItemModel.GetSurveySubModuleItemsByUserId(userId).ToArray();
         }
 
-        /// <summary>
-        /// The get quiz data by quiz id.
-        /// </summary>
-        /// <param name="surveyId">
-        /// The quiz id.
-        /// </param>
-        /// <returns>
-        /// The <see cref="SurveyDataDTO"/>.
-        /// </returns>
         public SurveyDataDTO GetSurveyDataBySurveyId(int surveyId)
         {
             return this.SurveyModel.GetSurveyDataBySurveyId(surveyId);
@@ -259,18 +153,6 @@ namespace EdugameCloud.WCFService
 
         #region Methods
 
-        /// <summary>
-        /// The convert DTO.
-        /// </summary>
-        /// <param name="itemDTO">
-        /// The user.
-        /// </param>
-        /// <param name="instance">
-        /// The instance.
-        /// </param>
-        /// <returns>
-        /// The <see cref="Survey"/>.
-        /// </returns>
         private Survey ConvertDto(SurveyDTO itemDTO, Survey instance)
         {
             instance = instance ?? new Survey();
@@ -287,34 +169,21 @@ namespace EdugameCloud.WCFService
             return instance;
         }
 
-        /// <summary>
-        /// The convert quiz and get service response.
-        /// </summary>
-        /// <param name="surveyDTO">
-        /// The survey DTO.
-        /// </param>
-        /// <param name="survey">
-        /// The survey.
-        /// </param>
-        /// <param name="quizModel">
-        /// The quiz model.
-        /// </param>
-        /// <returns>
-        /// The <see cref="SurveyDTO"/>.
-        /// </returns>
         private SurveyDTO ConvertQuizAndGetServiceResponse(
             SurveyDTO surveyDTO,
             Survey survey,
-            SurveyModel quizModel)
+            SurveyModel model)
         {
             survey = this.ConvertDto(surveyDTO, survey);
-            quizModel.RegisterSave(survey, true);
-            int companyId =
-                survey.With(x => x.SubModuleItem).With(x => x.SubModuleCategory).With(x => x.User).With(x => x.Company.Id);
+            model.RegisterSave(survey, true);
+            //int companyId =
+            //    survey.With(x => x.SubModuleItem).With(x => x.SubModuleCategory).With(x => x.User).With(x => x.Company.Id);
             //IoC.Resolve<RealTimeNotificationModel>().NotifyClientsAboutChangesInTable<Survey>(NotificationType.Update, companyId, survey.Id);
             return new SurveyDTO(survey);
         }
 
         #endregion
+
     }
+
 }
