e922a627601304e718ec580eade2fb9797ad3459
kniaziuk <kniaziuk@esynctraining.com>
2017-08-17 09:22:56 +0000

    ACLTI-1023 Add / Develop : LMS Haiku WIP
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@15753 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.Lti.Host/Global.asax.cs
+++ EdugameCloud.Lti.Host/Global.asax.cs
@@ -122,7 +122,8 @@ namespace EdugameCloud.Lti.Host
                 Castle.Windsor.Installer.Configuration.FromXml(new AssemblyResource("assembly://EdugameCloud.Lti.Sakai/EdugameCloud.Lti.Sakai.Windsor.xml")),
                 //Castle.Windsor.Installer.Configuration.FromXml(new AssemblyResource("assembly://EdugameCloud.Lti.DialogEdu/EdugameCloud.Lti.DialogEdu.Windsor.xml")),
                 //Castle.Windsor.Installer.Configuration.FromXml(new AssemblyResource("assembly://EdugameCloud.Lti.Bridge/EdugameCloud.Lti.Bridge.Windsor.xml"))
-                Castle.Windsor.Installer.Configuration.FromXml(new AssemblyResource("assembly://EdugameCloud.Lti.Schoology/EdugameCloud.Lti.Schoology.Windsor.xml"))
+                Castle.Windsor.Installer.Configuration.FromXml(new AssemblyResource("assembly://EdugameCloud.Lti.Schoology/EdugameCloud.Lti.Schoology.Windsor.xml")),
+                Castle.Windsor.Installer.Configuration.FromXml(new AssemblyResource("assembly://EdugameCloud.Lti.Haiku/EdugameCloud.Lti.Haiku.Windsor.xml"))
             );
 
             container.Install(new LtiWindsorInstaller());
