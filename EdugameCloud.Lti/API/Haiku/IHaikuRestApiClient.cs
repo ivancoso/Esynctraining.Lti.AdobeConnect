6c6218d02bea4ea1e43c492abc14bacce8311f22
kniaziuk <kniaziuk@esynctraining.com>
2017-08-17 09:20:52 +0000

    ACLTI-1023 Add / Develop : LMS Haiku WIP
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@15751 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.Lti/API/Haiku/IHaikuRestApiClient.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using EdugameCloud.Lti.Domain.Entities;
+using EdugameCloud.Lti.DTO;
+
+namespace EdugameCloud.Lti.API.Haiku
+{
+    public interface IHaikuRestApiClient
+    {
+        bool TestOauth(string lmsDomain, string consumerKey, string consumerSecret, string token, string tokenSecret);
+
+        List<LmsUserDTO> GetUsersForCourse(ILmsLicense lmsCompany, int courseId, out string error);
+    }
+}
