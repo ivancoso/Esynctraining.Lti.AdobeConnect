cfea6d396f013e4d83721658b94fdf32893d8e11
roman <romanp@esynctraining.com>
2017-09-21 11:15:30 +0000

    ACLTI-1143 added HttpClientWrapper.cs
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@15976 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ EdugameCloud.HttpClient/HttpClientWrapper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace EdugameCloud.HttpClient
+{
+    public class HttpClientWrapper
+    {
+        private readonly System.Net.Http.HttpClient _httpClient  = new System.Net.Http.HttpClient();
+
+        #region Constructors
+
+        public HttpClientWrapper(){}
+
+        public HttpClientWrapper(System.Net.Http.HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        #endregion Constructors
+
+        #region Public Methods
+
+        public string PostValues(string url, Dictionary<string, string> pairs)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+                throw new ArgumentException("Non-empty value expected", nameof(url));
+            if (pairs == null)
+                throw new ArgumentNullException(nameof(pairs));
+
+            var response = _httpClient.PostAsync(url, new FormUrlEncodedContent(pairs)).Result;
+            return response.Content.ReadAsStringAsync().Result;
+        }
+
+        public string DownloadString(string url)
+        {
+            HttpResponseMessage response = _httpClient.GetAsync(url).Result;
+            HttpContent content = response.Content;
+            return content.ReadAsStringAsync().Result;
+        }
+
+        public HttpResponseMessage UploadJsonString(string address, string data)
+        {
+            var content = new StringContent(data, Encoding.UTF8, "application/json");
+            return _httpClient.PostAsync(address, content).Result;
+        }
+
+        #endregion Public Methods
+    }
+}
