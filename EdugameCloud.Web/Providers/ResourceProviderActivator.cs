f764ff12f111e6cdb519a7bb0ed00dbbda21fca6
sergey <sergeyi@esynctraining.com>
2017-10-11 13:03:52 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@16325 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.Web/Providers/ResourceProviderActivator.cs
+++ EdugameCloud.Web/Providers/ResourceProviderActivator.cs
@@ -7,37 +7,11 @@
     using Castle.MicroKernel.ComponentActivator;
     using Castle.MicroKernel.Context;
 
-    /// <summary>
-    /// The resource provider activator.
-    /// </summary>
     public class ResourceProviderActivator : DefaultComponentActivator
     {
-        #region Constants
+        private const string ResourceProvideActivatorKey = "ResourceProvideActivator";
 
-        /// <summary>
-        /// The resource provide activator key.
-        /// </summary>
-        public const string ResourceProvideActivatorKey = "ResourceProvideActivator";
 
-        #endregion
-
-        #region Constructors and Destructors
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ResourceProviderActivator"/> class.
-        /// </summary>
-        /// <param name="model">
-        /// The model.
-        /// </param>
-        /// <param name="kernel">
-        /// The kernel.
-        /// </param>
-        /// <param name="onCreation">
-        /// The on creation.
-        /// </param>
-        /// <param name="onDestruction">
-        /// The on destruction.
-        /// </param>
         public ResourceProviderActivator(
             ComponentModel model, 
             IKernelInternal kernel, 
@@ -47,38 +21,18 @@
         {
         }
 
-        #endregion
 
-        #region Methods
-
-        /// <summary>
-        /// The create instance.
-        /// </summary>
-        /// <param name="context">
-        /// The context.
-        /// </param>
-        /// <param name="constructor">
-        /// The constructor.
-        /// </param>
-        /// <param name="arguments">
-        /// The arguments.
-        /// </param>
-        /// <returns>
-        /// The <see cref="object"/>.
-        /// </returns>
         protected override object CreateInstance(
             CreationContext context, ConstructorCandidate constructor, object[] arguments)
         {
-            if (HttpContext.Current != null && HttpContext.Current.User != null
-                && HttpContext.Current.User.Identity.IsAuthenticated)
+            var ctx = HttpContext.Current;
+            if (ctx != null && ctx.User != null && ctx.User.Identity.IsAuthenticated)
             {
-                return HttpContext.Current.Session[ResourceProvideActivatorKey]
-                       ?? (HttpContext.Current.Session[ResourceProvideActivatorKey] =
-                           new EGCResourceProvider());
+                return ctx.Session[ResourceProvideActivatorKey] ?? (ctx.Session[ResourceProvideActivatorKey] = new EGCResourceProvider());
             }
             return new EGCResourceProvider();
         }
 
-        #endregion
     }
+
 }
