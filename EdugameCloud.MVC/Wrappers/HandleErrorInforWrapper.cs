e9f4239988f20dcd94f80b19d64aa789c3105a0d
sergey <sergeyi@esynctraining.com>
2017-12-29 08:49:33 +0000

    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@16932 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.MVC/Wrappers/HandleErrorInforWrapper.cs
+++ EdugameCloud.MVC/Wrappers/HandleErrorInforWrapper.cs
@@ -3,49 +3,23 @@
     using System;
     using System.Web.Mvc;
 
-    /// <summary>
-    /// The handle error info wrapper.
-    /// </summary>
     public class HandleErrorInfoWrapper
     {
-        #region Constructors and Destructors
+        public string ActionName { get; }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="HandleErrorInfoWrapper"/> class.
-        /// </summary>
-        /// <param name="exception">
-        /// The exception.
-        /// </param>
-        /// <param name="actionResult">
-        /// The action result.
-        /// </param>
+        public string ControllerName { get; }
+
+        public Exception Exception { get; }
+
+
         public HandleErrorInfoWrapper(Exception exception, ActionResult actionResult)
         {
             var callInfo = actionResult.GetT4MVCResult();
-            this.Exception = exception;
-            this.ControllerName = callInfo.Controller;
-            this.ActionName = callInfo.Action;
+            Exception = exception;
+            ControllerName = callInfo.Controller;
+            ActionName = callInfo.Action;
         }
 
-        #endregion
-
-        #region Public Properties
-
-        /// <summary>
-        /// Gets the action name.
-        /// </summary>
-        public string ActionName { get; private set; }
-
-        /// <summary>
-        /// Gets the controller name.
-        /// </summary>
-        public string ControllerName { get; private set; }
-
-        /// <summary>
-        /// Gets the exception.
-        /// </summary>
-        public Exception Exception { get; private set; }
-
-        #endregion
     }
+
 }
