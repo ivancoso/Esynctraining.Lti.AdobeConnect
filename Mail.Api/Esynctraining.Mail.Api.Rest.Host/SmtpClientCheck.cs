0a82e09687fad63b3465a9df93eda8b015f2b267
sergey <sergeyi@esynctraining.com>
2018-04-06 11:04:27 +0000

    await _mail.SendEmailAsync
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@17439 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- Mail.Api/Esynctraining.Mail.Api.Rest.Host/SmtpClientCheck.cs
+++ Mail.Api/Esynctraining.Mail.Api.Rest.Host/SmtpClientCheck.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Esynctraining.Mail.Configuration;
 using Esynctraining.Mail.Configuration.Json;
 using Microsoft.Extensions.HealthChecks;
 
@@ -13,32 +10,35 @@ namespace Esynctraining.Mail.Api.Rest.Host
     {
         private readonly ISmtpClient _mail;
 
+
         public SmtpClientCheck(ISmtpClient mail)
         {
             _mail = mail ?? throw new ArgumentNullException(nameof(mail));
         }
 
-        public ValueTask<IHealthCheckResult> CheckAsync(CancellationToken cancellationToken = default(CancellationToken))
+
+        public async ValueTask<IHealthCheckResult> CheckAsync(CancellationToken cancellationToken = default(CancellationToken))
         {
             try
             {
-                var sendResult = _mail.SendEmailAsync(
+                var sendResult = await _mail.SendEmailAsync(
                     new SystemEmail { Email = "system@esynctraining.com" }, 
                     new[] { new SystemEmail { Email = "system@esynctraining.com" } }, 
                     "Subject", 
                     "Test Body", 
                     null, 
-                    null).Result;
+                    null);
 
-                if (sendResult)
-                    return new ValueTask<IHealthCheckResult>(HealthCheckResult.Healthy("SMTP Check: Healthy"));
-                else
-                    return new ValueTask<IHealthCheckResult>(HealthCheckResult.Unhealthy("SMTP Check: Unhealthy"));
+                return sendResult
+                    ? HealthCheckResult.Healthy("SMTP Check: Healthy")
+                    : HealthCheckResult.Unhealthy("SMTP Check: Unhealthy");
             }
             catch (Exception ex)
             {
-                return new ValueTask<IHealthCheckResult>(HealthCheckResult.Unhealthy($"SMTP Check: Exception during check: {ex.Message} {ex.GetType().FullName}"));
+                return HealthCheckResult.Unhealthy($"SMTP Check: Exception during check: {ex.Message} {ex.GetType().FullName}");
             }
         }
+
     }
+
 }
