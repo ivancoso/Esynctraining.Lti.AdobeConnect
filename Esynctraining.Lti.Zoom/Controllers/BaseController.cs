060ae6b564e58100f377a6edb2851d4be19cc6aa
roman <romanp@esynctraining.com>
2018-06-11 19:36:48 +0000

    ZLTI-115 added new project Esynctraining.Lti.Zoom.Host, Esynctraining.Lti.zoom
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@18066 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- Esynctraining.Lti.Zoom/Controllers/BaseController.cs
+++ Esynctraining.Lti.Zoom/Controllers/BaseController.cs
@@ -1,19 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Esynctraining.Core.Logging;
+﻿using Esynctraining.Core.Logging;
+using Esynctraining.Core.Providers;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace Esynctraining.Lti.Zoom.Controllers
 {
 
     public class BaseController : Controller
     {
-        protected ILogger _logger { get; }
+        protected readonly ILogger _logger;
+        protected readonly dynamic _settings;
+        private static bool? _isDebug;
 
-        public BaseController(ILogger logger)
+        protected bool IsDebug
+        {
+            get
+            {
+                if (_isDebug.HasValue)
+                {
+                    return _isDebug.Value;
+                }
+
+                bool val;
+                _isDebug = bool.TryParse(_settings.IsDebug, out val) && val;
+                return _isDebug.Value;
+            }
+        }
+
+
+        public BaseController(ILogger logger, ApplicationSettingsProvider settings)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _settings = settings ?? throw new ArgumentNullException(nameof(settings));
         }
 
     }
--- Esynctraining.Lti.Zoom/Controllers/LtiController.cs
+++ Esynctraining.Lti.Zoom/Controllers/LtiController.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Text;
 using Esynctraining.Lti.Zoom.DTO;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using Esynctraining.Core.Logging;
+using Esynctraining.Core.Providers;
 using Esynctraining.Core.Utils;
+using Esynctraining.Lti.Zoom.Extensions;
+using LtiLibrary.NetCore.Common;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using ILogger = Esynctraining.Core.Logging.ILogger;
 
 namespace Esynctraining.Lti.Zoom.Controllers
 {
@@ -14,7 +21,7 @@ namespace Esynctraining.Lti.Zoom.Controllers
 
         #region Constructors and Destructors
 
-        public LtiController(ILogger logger) : base(logger)
+        public LtiController(ILogger logger, ApplicationSettingsProvider settings) : base(logger, settings)
         {
             
         }
@@ -44,8 +51,58 @@ namespace Esynctraining.Lti.Zoom.Controllers
 
         public virtual async Task<ActionResult> LoginWithProvider(string provider, LtiParamDTO param)
         {
+            var methodTime = Stopwatch.StartNew();
+            var trace = new StringBuilder();
+
+            try
+            {
+                // TRICK: to save course_id in DB;
+                param.CalculateFields();
+                // Parse and validate the request
+                Request.CheckForRequiredLtiParameters();
+                var sw = Stopwatch.StartNew();
+
+
+
+            }
+            catch (LtiException ex)
+            {
+                _logger.Error("Lti exception", ex);
+                ViewBag.Message = $"Invalid LTI request. {ex.Message}";
+                if (!string.IsNullOrEmpty(param.launch_presentation_return_url))
+                {
+                    ViewBag.ReturnUrl = param.launch_presentation_return_url;
+                }
+                return View("~/Views/Lti/LtiError.cshtml");
+            }
+            //catch (Core.WarningMessageException ex)
+            //{
+            //    _logger.WarnFormat("[WarningMessageException] param:{0}.",
+            //        JsonSerializer.JsonSerialize(param));
+            //    this.ViewBag.Message = ex.Message;
+            //    return this.View("~/Views/Lti/LtiError.cshtml");
+            //}
+            catch (Exception ex)
+            {
+                _logger.ErrorFormat(ex, "LoginWithProvider exception. oauth_consumer_key:{0}.", param.oauth_consumer_key);
+                this.ViewBag.DebugError = IsDebug ? (ex.Message + ex.StackTrace) : string.Empty;
+                return this.View("~/Views/Lti/LtiError.cshtml");
+            }
+            {
+                methodTime.Stop();
+                var time = methodTime.Elapsed;
+                if (time > TimeSpan.FromSeconds(int.Parse((string)_settings.Monitoring_MaxLoginTime)))
+                {
+                    var monitoringLog = IoC.Resolve<ILogger>("Monitoring");
+
+                    monitoringLog.ErrorFormat("LoginWithProvider takes more than {0} seconds. Time: {1}. Details: {2}.",
+                        _settings.Monitoring_MaxLoginTime.ToString(),
+                        time.ToString(), trace.ToString());
+                }
+            }
+
             ViewData["Message"] = provider;
-            return this.View("About");
+            return this.View("~/Views/Lti/About.cshtml");
         }
     }
 }
