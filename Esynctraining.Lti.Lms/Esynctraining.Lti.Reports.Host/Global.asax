2bdc7a1798c87987bba067b29da404e8eaa982f8
roman <romanp@esynctraining.com>
2018-06-20 12:25:24 +0000

    clean
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@18209 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- Esynctraining.Lti.Lms/Esynctraining.Lti.Reports.Host/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EdugameCloud.Lti.Reports.Host.MvcApplication" Language="C#" %>
--- Esynctraining.Lti.Lms/Esynctraining.Lti.Reports.Host/Models/SessionParticipantDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EdugameCloud.Lti.Reports.Host.Models
-{
-    public class SessionParticipantDto
-    {
-        public string ParticipantName { get; set; }
-        public string DateTimeEntered { get; set; }
-        public string DateTimeLeft { get; set; }
-    }
-}
