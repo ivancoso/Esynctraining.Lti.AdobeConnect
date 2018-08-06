a1d4cdc8bbaad6a200001149ee8e7cb4fabf759f
roman <romanp@esynctraining.com>
2018-08-06 12:44:49 +0000

    ZLTI-303 Only a Canvas user who has a Zoom account with the same email address as their Canvas email address is authorized to use Zoom within Canvas.
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@18652 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- Esynctraining.Lti.Zoom/Controllers/LtiController.cs
+++ Esynctraining.Lti.Zoom/Controllers/LtiController.cs
@@ -400,7 +400,7 @@ namespace Esynctraining.Lti.Zoom.Controllers
 
             var inactiveUsers = _userService.GetUsers(UserStatuses.Inactive);
             if (inactiveUsers.Users.Any(u => u.Email.Equals(paramLisPersonContactEmailPrimary, StringComparison.CurrentCultureIgnoreCase)))
-                throw new LtiException("User has an account, but it is inactiv.");
+                throw new LtiException("User has an account, but it is inactive.");
 
             var pendingUsers = _userService.GetUsers(UserStatuses.Pending);
             if (pendingUsers.Users.Any(u => u.Email.Equals(paramLisPersonContactEmailPrimary, StringComparison.CurrentCultureIgnoreCase)))
