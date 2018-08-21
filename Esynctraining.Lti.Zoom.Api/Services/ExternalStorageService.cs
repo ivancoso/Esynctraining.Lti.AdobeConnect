70d6186df3ae61474ba130ea9827f5046e9a94d2
ivan <ivan@esynctraining.com>
2018-08-21 06:03:26 +0000

    https://jira.esynctraining.com/browse/ZLTI-334 checking Kaltura license settings
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@18877 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- Esynctraining.Lti.Zoom.Api/Services/ExternalStorageService.cs
+++ Esynctraining.Lti.Zoom.Api/Services/ExternalStorageService.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Esynctraining.Core.Domain;
+using Esynctraining.Core.Logging;
+using Esynctraining.Lti.Lms.Common.Constants;
 using Esynctraining.Lti.Zoom.Api.Dto;
 using Esynctraining.Lti.Zoom.Domain;
 using Microsoft.EntityFrameworkCore;
@@ -12,17 +14,22 @@ namespace Esynctraining.Lti.Zoom.Api.Services
     {
         private readonly ZoomDbContext _dbContext;
         private readonly KalturaService _kalturaService;
+        private readonly ILmsLicenseAccessor _licenseAccessor;
+        private readonly ILogger _logger;
 
-        public ExternalStorageService(ZoomDbContext dbContext, KalturaService kalturaService)
+        public ExternalStorageService(ZoomDbContext dbContext, KalturaService kalturaService,
+            ILmsLicenseAccessor licenseAccessor, ILogger logger)
         {
             _dbContext = dbContext;
             _kalturaService = kalturaService;
+            _licenseAccessor = licenseAccessor;
+            _logger = logger;
         }
 
         public async Task<OperationResult> AddExternalFileRecord(LmsCourseMeeting dbMeeting,
             int providerId, string providerFileRecordId)
         {
-            ExternalStorageProvider provider = (ExternalStorageProvider)providerId;
+            ExternalStorageProvider provider = (ExternalStorageProvider) providerId;
             if (await _dbContext.ExternalFiles.AnyAsync(x => x.Meeting.Id == dbMeeting.Id
                                                              && x.ProviderId == provider &&
                                                              x.ProviderFileRecordId == providerFileRecordId))
@@ -32,7 +39,12 @@ namespace Esynctraining.Lti.Zoom.Api.Services
             if (!beforeSaveResult.IsSuccess)
                 return beforeSaveResult;
 
-            var fileInfo = new ExternalFileInfo{Meeting = dbMeeting, ProviderId = (ExternalStorageProvider)providerId, ProviderFileRecordId = providerFileRecordId };
+            var fileInfo = new ExternalFileInfo
+            {
+                Meeting = dbMeeting,
+                ProviderId = (ExternalStorageProvider) providerId,
+                ProviderFileRecordId = providerFileRecordId
+            };
             await _dbContext.ExternalFiles.AddAsync(fileInfo);
             await _dbContext.SaveChangesAsync();
             return OperationResult.Success();
@@ -67,10 +79,31 @@ namespace Esynctraining.Lti.Zoom.Api.Services
 
         public async Task<IEnumerable<ExternalRecordingsDto>> GetExternalFileRecords(int meetingId)
         {
-            var fileInfos = await _dbContext.ExternalFiles.Where(x => x.Meeting.Id == meetingId).ToListAsync();
+            var providers = await GetEnabledStorageProviders();
+            if (!providers.Any())
+            {
+                return new List<ExternalRecordingsDto>();
+            }
+
+            var fileInfos = await _dbContext.ExternalFiles
+                .Where(x => x.Meeting.Id == meetingId && providers.Any(p => p == x.ProviderId)).ToListAsync();
             var result = fileInfos.GroupBy(x => x.ProviderId).Select(x =>
                 new ExternalRecordingsDto {ProviderId = x.Key, RecordingIds = x.Select(ef => ef.ProviderFileRecordId)});
             return result.ToList();
+        }
+
+        private async Task<List<ExternalStorageProvider>> GetEnabledStorageProviders()
+        {
+            var result = new List<ExternalStorageProvider>();
+            var licenseDto = await _licenseAccessor.GetLicense();
+
+            var enabledKaltura = licenseDto.GetSetting<bool>(LmsLicenseSettingNames.EnableKaltura);
+            if (enabledKaltura)
+            {
+                result.Add(ExternalStorageProvider.Kaltura);
+            }
+
+            return result;
         }
     }
 }
