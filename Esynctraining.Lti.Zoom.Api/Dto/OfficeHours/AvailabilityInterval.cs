c4650e3cbfdcff31741d5e710e5fab86bc55a400
ivan <ivan@esynctraining.com>
2018-09-04 07:40:34 +0000

    Validate slot intervals
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@19138 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- Esynctraining.Lti.Zoom.Api/Dto/OfficeHours/AvailabilityInterval.cs
+++ Esynctraining.Lti.Zoom.Api/Dto/OfficeHours/AvailabilityInterval.cs
@@ -1,8 +1,15 @@
-﻿namespace Esynctraining.Lti.Zoom.Api.Dto.OfficeHours
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Esynctraining.Lti.Zoom.Api.Dto.OfficeHours
 {
     public class AvailabilityInterval
     {
+        //Interval Start and End times are in minutes from the beginning of the day, so values are 0-1440
+
+        [Range(0, 24*60, ErrorMessage = "Interval Start is out of range")]
         public int Start { get; set; }
+
+        [Range(0, 24*60, ErrorMessage = "Interval End is out of range")]
         public int End { get; set; }
     }
 }
