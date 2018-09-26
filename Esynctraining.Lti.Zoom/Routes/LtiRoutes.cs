e83137f47cd2c99621a6e6d1a7be5852f3327612
roman <romanp@esynctraining.com>
2018-09-26 11:26:20 +0000

    ZLTI-455 added JoinMeetingMobile
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@19445 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- Esynctraining.Lti.Zoom/Routes/LtiRoutes.cs
+++ Esynctraining.Lti.Zoom/Routes/LtiRoutes.cs
@@ -10,6 +10,7 @@ namespace Esynctraining.Lti.Zoom.Routes
             routes.MapRoute("LtiOAuthLogin", "{provider}-login", new { controller = RouteConstants.LtiControllerName, action = "LoginWithProvider" });
             routes.MapRoute("LtiOAuthCallback", "oauth-callback", new { controller = RouteConstants.LtiControllerName, action = RouteConstants.AuthCallbackActionName });
             routes.MapRoute("JoinMeeting", "meetings/{meetingId}/join", new { controller = RouteConstants.LtiControllerName, action = RouteConstants.JoinMeetingActionName });
+            routes.MapRoute("JoinMeetingMobile", "meetings/{meetingId}/joinmobile", new { controller = RouteConstants.LtiControllerName, action = RouteConstants.JoinMeetingMobileActionName });
             routes.MapRoute("ReportBySession", "reports/meetings/{meetingId}/by-sessions/download", new { controller = RouteConstants.ReportsControllerName, action = RouteConstants.DownloadReportActionName });
             routes.MapRoute("ReportBySessionDetails", "reports/meetings/{meetingId}/details/{meetingSessionId}/download", new { controller = RouteConstants.ReportsControllerName, action = RouteConstants.DownloadCsvReportActionName });
             routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
@@ -22,6 +23,7 @@ namespace Esynctraining.Lti.Zoom.Routes
         public const string ReportsControllerName = "Reports";
         public const string AuthCallbackActionName = "AuthenticationCallback";
         public const string JoinMeetingActionName = "JoinMeeting";
+        public const string JoinMeetingMobileActionName = "JoinMeetingMobile";
         public const string DownloadReportActionName = "DownloadReport";
         public const string DownloadCsvReportActionName = "DownloadReportSessionDetails";
     }
