da8f96eb40e98fe6641fead8f30ff992e78a455d
roman <romanp@esynctraining.com>
2018-09-26 10:41:02 +0000

    ZLTI-455 added JoinMeetingMobile
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@19440 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ Esynctraining.Lti.Zoom.Api/Dto/JoinLinkParamDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Esynctraining.Lti.Zoom.Api.Dto
+{
+    public class JoinLinkParamDto
+    {
+        public string ConfoNo { get; set; }
+        public string Uname { get; set; }
+        public string Uid { get; set; }
+        public string Tk { get; set; }
+    }
+}
