5a9e81869beca06decd8d2961eade8d008298296
roman <romanp@esynctraining.com>
2018-10-04 09:04:12 +0000

    ZLTI-473 Implement server side to Support Study Groups for Teachers and Students
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@19526 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ Esynctraining.Lti.Zoom.Api/Dto/Enums/CourseMeetingType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Esynctraining.Lti.Zoom.Api.Dto.Enums
+{
+    public enum CourseMeetingType
+    {
+        Instant = 1,
+        OfficeHour = 2,
+        StudyGroup = 3,
+    }
+}
