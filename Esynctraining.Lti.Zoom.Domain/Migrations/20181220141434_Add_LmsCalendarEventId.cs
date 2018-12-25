c1e98dbacc0c903552c1b4cb75185eaf5c45eee0
roman <romanp@esynctraining.com>
2018-12-25 13:53:20 +0000

    ZLTI-560 added hack
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@20164 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ Esynctraining.Lti.Zoom.Domain/Migrations/20181220141434_Add_LmsCalendarEventId.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Esynctraining.Lti.Zoom.Domain.Migrations
+{
+    public partial class Add_LmsCalendarEventId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LmsCalendarEventId",
+                table: "LmsMeetingSession",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LmsCalendarEventId",
+                table: "LmsCourseMeeting",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LmsCalendarEventId",
+                table: "LmsMeetingSession");
+
+            migrationBuilder.DropColumn(
+                name: "LmsCalendarEventId",
+                table: "LmsCourseMeeting");
+        }
+    }
+}
