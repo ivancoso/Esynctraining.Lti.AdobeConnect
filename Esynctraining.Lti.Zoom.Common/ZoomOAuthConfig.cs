610dab72969def242213b1e34034302abc4e6f35
ivan <ivan@esynctraining.com>
2019-01-25 14:07:06 +0000

    Zoom OAuth
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@20370 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ Esynctraining.Lti.Zoom.Common/ZoomOAuthConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace Esynctraining.Lti.Zoom.Common
+{
+    public class ZoomOAuthConfig
+    {
+        public string ClientID { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ Esynctraining.Lti.Zoom.Common/ZoomOAuthOptionsFromLicenseAccessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Esynctraining.Lti.Lms.Common.Constants;
+using Esynctraining.Lti.Zoom.Common.Services;
+using Esynctraining.Zoom.ApiWrapper.OAuth;
+
+namespace Esynctraining.Lti.Zoom.Common
+{
+    public class ZoomOAuthOptionsFromLicenseAccessor : IZoomOAuthOptionsAccessor
+    {
+        private readonly ILmsLicenseAccessor _licenseAccessor;
+        private readonly ZoomOAuthConfig _zoomOAuthConfig;
+
+        public ZoomOAuthOptionsFromLicenseAccessor(ILmsLicenseAccessor licenseAccessor, ZoomOAuthConfig zoomOAuthConfig)
+        {
+            _licenseAccessor = licenseAccessor ?? throw new ArgumentNullException(nameof(licenseAccessor));
+        }
+
+        public async Task<ZoomOAuthOptions> GetOptions()
+        {
+            var license = await _licenseAccessor.GetLicense();
+
+            //todo: check & refresh token if necessary
+            return new ZoomOAuthOptions
+            {
+                AccessToken = license.GetSetting<string>(LmsLicenseSettingNames.ZoomApiAccessToken),
+            };
+        }
+    }
+}
