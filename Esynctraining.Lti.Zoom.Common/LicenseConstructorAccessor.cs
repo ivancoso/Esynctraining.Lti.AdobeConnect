cdbe2e5abf03fae3efeadf9cfd32073d4376b07c
roman <romanp@esynctraining.com>
2019-01-30 09:22:43 +0000

    ZLTI-581 move to Oauth2
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@20381 41667c05-fd31-8a45-9ea7-b4ae18c5af78

---
+++ Esynctraining.Lti.Zoom.Common/LicenseConstructorAccessor.cs
@@ -0,0 +1,21 @@
+﻿using Esynctraining.Lti.Zoom.Common.Dto;
+using Esynctraining.Lti.Zoom.Common.Services;
+using System.Threading.Tasks;
+
+namespace Esynctraining.Lti.Zoom.Common
+{
+    public class LicenseConstructorAccessor : ILmsLicenseAccessor
+    {
+        private LmsLicenseDto _license;
+
+        public LicenseConstructorAccessor(LmsLicenseDto license)
+        {
+            _license = license;
+        }
+
+        public async Task<LmsLicenseDto> GetLicense()
+        {
+            return _license;
+        }
+    }
+}
