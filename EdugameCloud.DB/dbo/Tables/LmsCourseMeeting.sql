359ba00c783d2e7f423501bf4fb1d5a802ca2457
ivan <ivan@esynctraining.com>
2019-07-19 12:06:03 +0000

    ACLTI-2115 DB updates
    git-svn-id: https://svn.esynctraining.com/svn/esynctraining@20863 41667c05-fd31-8a45-9ea7-b4ae18c5af78

--- EdugameCloud.DB/dbo/Tables/LmsCourseMeeting.sql
+++ EdugameCloud.DB/dbo/Tables/LmsCourseMeeting.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[LmsCourseMeeting] (
     [lmsCourseMeetingId]        INT             IDENTITY (1, 1) NOT NULL,
-    [courseId]                  INT             NOT NULL,
+    [courseId]                  NVARCHAR (50)   NOT NULL,
     [scoId]                     NVARCHAR (50)   NULL,
     [companyLmsId]              INT             NOT NULL,
     [lmsMeetingTypeId]          INT             NOT NULL,
@@ -12,13 +12,15 @@
     [audioProfileId]            NVARCHAR (50)   NULL,
     [enableDynamicProvisioning] BIT             CONSTRAINT [DF_LmsCourseMeeting_enableDynamicProvisioning] DEFAULT ((0)) NOT NULL,
     [audioProfileProvider]      NVARCHAR (50)   NULL,
-    [lmsCalendarEventId] NVARCHAR(50) NULL, 
+    [lmsCalendarEventId]        NVARCHAR (50)   NULL,
     CONSTRAINT [PK_LmsCourseMeeting] PRIMARY KEY CLUSTERED ([lmsCourseMeetingId] ASC) WITH (FILLFACTOR = 80),
     CONSTRAINT [FK_LmsCourseMeeting_CompanyLms] FOREIGN KEY ([companyLmsId]) REFERENCES [dbo].[CompanyLms] ([companyLmsId]),
     CONSTRAINT [FK_LmsCourseMeeting_LmsMeetingType] FOREIGN KEY ([lmsMeetingTypeId]) REFERENCES [dbo].[LmsMeetingType] ([lmsMeetingTypeId]),
     CONSTRAINT [FK_LmsCourseMeeting_LmsUser] FOREIGN KEY ([ownerId]) REFERENCES [dbo].[LmsUser] ([lmsUserId]),
     CONSTRAINT [FK_LmsCourseMeeting_OfficeHours] FOREIGN KEY ([officeHoursId]) REFERENCES [dbo].[OfficeHours] ([officeHoursId])
 );
+
+
 
 
 GO
--- EdugameCloud.DB/dbo/Tables/LmsUserParameters.sql
+++ EdugameCloud.DB/dbo/Tables/LmsUserParameters.sql
@@ -1,17 +1,19 @@
 ﻿CREATE TABLE [dbo].[LmsUserParameters] (
-	[lmsUserParametersId]	INT				NOT NULL	IDENTITY (1, 1),
-	[wstoken]				NVARCHAR(128)		NULL,
-	[course]				INT				NOT NULL,
-	[acId]					NVARCHAR(10)	NOT NULL,
-	[lmsUserId]				INT					NULL,
-	[companyLmsId]			INT					NULL,
-	[courseName]			NVARCHAR(4000)		NULL,
-	[userEmail]				NVARCHAR(254)		NULL,
-	[lastLoggedIn]			DATETIME			NULL,
-	CONSTRAINT [PK_LmsUserParameters] PRIMARY KEY CLUSTERED ([lmsUserParametersId] ASC),
-	CONSTRAINT [FK_LmsUserParameters_CompanyLms] FOREIGN KEY ([companyLmsId]) REFERENCES [dbo].[CompanyLms] ([companyLmsId]) ON DELETE CASCADE,
-	CONSTRAINT [FK_LmsUserParameters_LmsUser] FOREIGN KEY ([lmsUserId]) REFERENCES [dbo].[LmsUser] ([lmsUserId])
+    [lmsUserParametersId] INT             IDENTITY (1, 1) NOT NULL,
+    [wstoken]             NVARCHAR (128)  NULL,
+    [course]              NVARCHAR (50)   NOT NULL,
+    [acId]                NVARCHAR (10)   NOT NULL,
+    [lmsUserId]           INT             NULL,
+    [companyLmsId]        INT             NULL,
+    [courseName]          NVARCHAR (4000) NULL,
+    [userEmail]           NVARCHAR (254)  NULL,
+    [lastLoggedIn]        DATETIME        NULL,
+    CONSTRAINT [PK_LmsUserParameters] PRIMARY KEY CLUSTERED ([lmsUserParametersId] ASC) WITH (FILLFACTOR = 80),
+    CONSTRAINT [FK_LmsUserParameters_CompanyLms] FOREIGN KEY ([companyLmsId]) REFERENCES [dbo].[CompanyLms] ([companyLmsId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_LmsUserParameters_LmsUser] FOREIGN KEY ([lmsUserId]) REFERENCES [dbo].[LmsUser] ([lmsUserId])
 );
+
+
 
 
 
